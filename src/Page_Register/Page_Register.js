9dabe4ff459f301e2fa3899a6daf544662bef3df
kobkij <Kobkij@globofoods.com>
2022-04-21 11:21:10 +0700

    Add Page Register

---
+++ src/Page_Register/Page_Register.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Register from '../components/Register/Register'
+
+const Page_Register = () => {
+  return (
+    <Register />
+  )
+}
+
+export default Page_Register
