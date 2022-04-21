9aa67a9cbcca5e13dfac994f87f4f881b08e132b
kobkij <Kobkij@globofoods.com>
2022-04-21 10:51:23 +0700

    Add Page Login

---
+++ src/Page_Login/Page_Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Login from "../components/Login/Login";
+
+const Page_Login = () => {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default Page_Login
