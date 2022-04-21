42feac5cafd513687d494200513114e09e6048a9
kobkij <Kobkij@globofoods.com>
2022-04-21 14:04:09 +0700

    commit

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,29 +1,21 @@
 import React from 'react'
+import "../CSS/Content.css"
 
 const Register = () => {
   return (
       <div className="hold-transition register-page">
           <div className="register-box">
               <div className="register-logo">
-                  <a href="../../index2.html"><b>Admin</b>LTE</a>
+                  <b>Register</b>
               </div>
               <div className="card">
                   <div className="card-body register-card-body">
-                      <p className="login-box-msg">Register a new membership</p>
-                      <form action="../../index.html" method="post">
+                      <form action="#" method="post">
                           <div className="input-group mb-3">
                               <input type="text" className="form-control" placeholder="Full name" />
                               <div className="input-group-append">
                                   <div className="input-group-text">
                                       <span className="fas fa-user" />
-                                  </div>
-                              </div>
-                          </div>
-                          <div className="input-group mb-3">
-                              <input type="email" className="form-control" placeholder="Email" />
-                              <div className="input-group-append">
-                                  <div className="input-group-text">
-                                      <span className="fas fa-envelope" />
                                   </div>
                               </div>
                           </div>
@@ -36,39 +28,48 @@ const Register = () => {
                               </div>
                           </div>
                           <div className="input-group mb-3">
-                              <input type="password" className="form-control" placeholder="Retype password" />
+                              <input type="password" className="form-control" placeholder="Confirm password" />
                               <div className="input-group-append">
                                   <div className="input-group-text">
                                       <span className="fas fa-lock" />
                                   </div>
                               </div>
                           </div>
-                          <div className="row">
-                              <div className="col-8">
-                                  <div className="icheck-primary">
-                                      <input type="checkbox" id="agreeTerms" name="terms" defaultValue="agree" />
-                                      <label htmlFor="agreeTerms">
-                                          I agree to the <a href="#">terms</a>
-                                      </label>
+                          <div className="input-group mb-3">
+                              <input type="text" className="form-control" placeholder="Name" />
+                              <div className="input-group-append">
+                                  <div className="input-group-text">
                                   </div>
                               </div>
+                          </div>
+                          <div className="input-group mb-3">
+                              <input type="text" className="form-control" placeholder="Last Name" />
+                              <div className="input-group-append">
+                                  <div className="input-group-text">
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="input-group mb-3">
+                              <select className="form-control select2">
+                                  <option selected="selected">--เลือก Role--</option>
+                                  <option>Alabama</option>
+                                  <option>Alaska</option>
+                                  <option>California</option>
+                                  <option>Delaware</option>
+                                  <option>Tennessee</option>
+                                  <option>Texas</option>
+                                  <option>Washington</option>
+                              </select>                             
+                          </div>
+                          <div className="row">
                               <div className="col-4">
                                   <button type="submit" className="btn btn-primary btn-block">Register</button>
                               </div>
+                              <div className="col-4">
+                                  <a href='/Login'><button type="button" className="btn btn-info btn-block">Back</button></a>
+                              </div>
                           </div>
                       </form>
-                      <div className="social-auth-links text-center">
-                          <p>- OR -</p>
-                          <a href="#" className="btn btn-block btn-primary">
-                              <i className="fab fa-facebook mr-2" />
-                              Sign up using Facebook
-                          </a>
-                          <a href="#" className="btn btn-block btn-danger">
-                              <i className="fab fa-google-plus mr-2" />
-                              Sign up using Google+
-                          </a>
-                      </div>
-                      <a href="login.html" className="text-center">I already have a membership</a>
                   </div>
               </div>
           </div>
