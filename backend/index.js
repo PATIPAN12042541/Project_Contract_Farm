62d79d43619e68ab7067c13f638b53a9f04a26d0
kobkij <Kobkij@globofoods.com>
2022-04-21 15:18:50 +0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import db from "./config/database.js";
+import RoleRoutes from "./routes/RoleRoutes.js";
 import cors from "cors";
-import db from "./config/Database.js";
-import router from "./routes/index.js";
-dotenv.config();
+ 
 const app = express();
  
-app.use(cors({ credentials:true, origin:'http://localhost:3000' }));
-app.use(cookieParser());
-app.use(express.json());
-app.use(router);
+try {
+    await db.authenticate();
+    console.log('Database connected...');
+} catch (error) {
+    console.error('Connection error:', error);
+}
  
-app.listen(5000, ()=> console.log('Server running at port 4000'));
+app.use(cors());
+app.use(express.json());
+app.use('/rolegroups', RoleRoutes);
+ 
+app.listen(5000, () => console.log('Server running at port 4000'));
