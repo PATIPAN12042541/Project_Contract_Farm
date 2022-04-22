1c567fca6ae71eaddc6006e02bb2562d71c32be9
kobkij <Kobkij@globofoods.com>
2022-04-22 13:14:42 +0700

    commit

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -18,8 +18,8 @@ const Register = () => {
     },[])
 
     const getRole = async() => {
-        //const response = await axios.get('http://node31023-env-2823146.th1.proen.cloud:4000/role_group');
-        const response = await axios.get(process.env.REACT_APP_API_URL+"/role_group");
+        const response = await axios.get('http://node31023-env-2823146.th1.proen.cloud:4000/role_group');
+        //const response = await axios.get(process.env.REACT_APP_API_URL+"/role_group");
         setRoleGroup(response.data);
     }
 
