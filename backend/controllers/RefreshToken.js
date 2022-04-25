a85d53880281b1cbd1e5ab4497001e9606e644a5
kobkij <Kobkij@globofoods.com>
2022-04-25 15:16:28 +0700

    commit

--- backend/controllers/RefreshToken.js
+++ backend/controllers/RefreshToken.js
@@ -4,7 +4,8 @@ import jwt from "jsonwebtoken";
 export const refreshToken = async(req, res) => {
     try {
         const refreshToken = req.cookies.refreshToken;
-        if(!refreshToken) return res.sendStatus(401);
+        console.log("refreshToken : "+refreshToken);
+        /*if(!refreshToken) return res.sendStatus(401);
         const user = await Users.findAll({
             where:{
                 refresh_token: refreshToken
@@ -20,7 +21,7 @@ export const refreshToken = async(req, res) => {
                 expiresIn: '15s'
             });
             res.json({ accessToken });
-        });
+        });*/
     } catch (error) {
         console.log(error);
     }
