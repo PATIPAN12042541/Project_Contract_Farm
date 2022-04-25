777e97e43112ecbb249069c97633f4f0a7e3e44d
kobkij <Kobkij@globofoods.com>
2022-04-25 09:35:52 +0700

    commit

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 const db = new Sequelize('contract_farm', 'ict', '@Loboadmin1', {
-    host: process.env.HOST_NAME,
+    host: '127.0.0.1',
     dialect: "mysql"
 });
  export default db;
