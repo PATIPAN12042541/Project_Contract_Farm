b83e81628b95e92f1b03a12504f86a084a47a9be
kobkij <Kobkij@globofoods.com>
2022-04-25 16:20:51 +0700

    commit

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -6,8 +6,8 @@ import { refreshToken } from "../controllers/RefreshToken.js";
 
 const router = express.Router();
 
-router.get('/check_users', verifyToken, getUsers);
-router.get('/', refreshToken);
+router.get('/', verifyToken, getUsers);
+router.get('/token', refreshToken);
 router.post('/register', Register);
 router.post('/login', Login);
 router.delete('/logout', Logout);
