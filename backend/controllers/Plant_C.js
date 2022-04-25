980c809f4039566136e060b3ed91430a2a30eab3
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-25 17:43:39 +0700

    c

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -3,7 +3,7 @@ import PlantDetail from "../models/PlantDetail_M.js";
 
 export const getPlant = async (req, res) => {
   try {
-    Plant.hasOne(PlantDetail, { primaryKey: "id_plant" });
+    Plant.hasMany(PlantDetail, { primaryKey: "id_plant" });
     PlantDetail.belongsTo(Plant, { foreignKey: "id_plant" });
 
     const plant = await Plant.findAll({ include: PlantDetail });
