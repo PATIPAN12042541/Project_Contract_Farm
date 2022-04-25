2d63a1194b334db553b443ecc8f62694ce482314
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-25 16:56:57 +0700

    commit

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -3,8 +3,8 @@ import PlantDetail from "../models/PlantDetail_M.js";
 
 export const getPlant = async (req, res) => {
   try {
-    Plant.hasMany(PlantDetail);
-    PlantDetail.belongsTo(Plant);
+    PlantDetail.hasMany(Plant);
+    Plant.belongsTo(PlantDetail);
 
     const plant = await Plant.findAll({ include: PlantDetail });
     res.json(plant);
