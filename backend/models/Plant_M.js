cfe1bd837e8fbcf0a37b9332352268f0a42040af
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-25 15:53:14 +0700

    commit

--- backend/models/Plant_M.js
+++ backend/models/Plant_M.js
@@ -3,57 +3,30 @@ import db from "../config/Database.js";
 const { DataTypes } = Sequelize;
 
 const Plant = db.define(
-    "plant",
-    {
-      id_plant: {
-        type: DataTypes.INTEGER,
-      },
-      name_plant: {
-        type: DataTypes.STRING,
-      },
-      start_date_plant: {
-        type: DataTypes.DATE,
-      },
-      end_date_plant: {
-        type: DataTypes.DATE,
-      },
-      plant_image: {
-        type: DataTypes.STRING,
-      },
+  "plant",
+  {
+    id_plant: {
+      type: DataTypes.INTEGER,
     },
-    {
-      freezeTableName: true,
-    }
-  ),
-  PlantDetail = db.define(
-    "plant_detail",
-    {
-      id_plant: {
-        type: DataTypes.INTEGER,
-      },
-      id_name_plant: {
-        type: DataTypes.STRING,
-      },
-      quantity_chemical: {
-        type: DataTypes.INTEGER,
-      },
-      unit: {
-        type: DataTypes.STRING,
-      },
-      note: {
-        type: DataTypes.STRING,
-      },
-      last_update: {
-        type: DataTypes.DATE,
-      },
+    name_plant: {
+      type: DataTypes.STRING,
     },
-    {
-      freezeTableName: true,
-    }
-  );
+    start_date_plant: {
+      type: DataTypes.DATE,
+    },
+    end_date_plant: {
+      type: DataTypes.DATE,
+    },
+    plant_image: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
 
-PlantDetail.belongsTo(Plant);
-Plant.hasMany(PlantDetail)(async () => {
+(async () => {
   await db.sync();
 })();
 export default Plant;
