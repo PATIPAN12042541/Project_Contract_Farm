dba278c590f34b2516ede7135aca574c4192a01b
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-25 16:28:38 +0700

    commit

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -2,7 +2,7 @@ import Plant from "../models/Plant_M.js";
 
 export const getPlant = async (req, res) => {
   try {
-    const plant = await Plant.findAll({ include: Plant });
+    const plant = await Plant.findAll({ include: PlantDetail });
     res.json(plant);
   } catch (error) {
     res.json({ message: error.message });
--- backend/models/Plant_M.js
+++ backend/models/Plant_M.js
@@ -3,61 +3,60 @@ import db from "../config/Database.js";
 const { DataTypes } = Sequelize;
 
 const Plant = db.define(
-  "plant",
-  {
-    id_plant: {
-      type: DataTypes.INTEGER,
+    "plant",
+    {
+      id_plant: {
+        type: DataTypes.INTEGER,
+      },
+      name_plant: {
+        type: DataTypes.STRING,
+      },
+      start_date_plant: {
+        type: DataTypes.DATE,
+      },
+      end_date_plant: {
+        type: DataTypes.DATE,
+      },
+      plant_image: {
+        type: DataTypes.STRING,
+      },
     },
-    name_plant: {
-      type: DataTypes.STRING,
+    {
+      timestamps: false,
+      freezeTableName: true,
+    }
+  ),
+  PlantDetail = db.define(
+    "plant_detail",
+    {
+      id_plant: {
+        type: DataTypes.INTEGER,
+      },
+      id_name_plant: {
+        type: DataTypes.STRING,
+      },
+      quantity_chemical: {
+        type: DataTypes.INTEGER,
+      },
+      unit: {
+        type: DataTypes.STRING,
+      },
+      note: {
+        type: DataTypes.STRING,
+      },
+      last_update: {
+        type: DataTypes.DATE,
+      },
     },
-    start_date_plant: {
-      type: DataTypes.DATE,
-    },
-    end_date_plant: {
-      type: DataTypes.DATE,
-    },
-    plant_image: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    freezeTableName: true,
-  }
-);
+    {
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
 
-const PlantDetail = db.define(
-  "plant_detail",
-  {
-    id_plant: {
-      type: DataTypes.INTEGER,
-    },
-    id_name_plant: {
-      type: DataTypes.STRING,
-    },
-    quantity_chemical: {
-      type: DataTypes.INTEGER,
-    },
-    unit: {
-      type: DataTypes.STRING,
-    },
-    note: {
-      type: DataTypes.STRING,
-    },
-    last_update: {
-      type: DataTypes.DATE,
-    },
-  },
-  {
-    timestamps: false,
-  },
-  {
-    freezeTableName: true,
-  }
-);
+Plant.hasMany(PlantDetail);
+PlantDetail.belongsTo(Plant);
 
-Plant.hasMany(PlantDetail, { as: "plant_detail", foreignKey: "id_plant" });
-PlantDetail.belongsTo(Plant, { foreignKey: "id_plant" });
 
 (async () => {
   await db.sync();
