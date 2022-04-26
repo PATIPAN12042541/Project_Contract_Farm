d30f5b84a762cfd7ca7d9e1d59fb8eec9ae25b48
kobkij <Kobkij@globofoods.com>
2022-04-26 08:51:34 +0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ try {
 //app.use(cors({ credentials:true, origin:'*' }));
 
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); //หรือใส่แค่เฉพาะ domain ที่ต้องการได้
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
