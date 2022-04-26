3d074007cc9aea310d47b0b7179af5745a9dc108
kobkij <Kobkij@globofoods.com>
2022-04-26 15:44:52 +0700

    commit

--- backend/controllers/RoleGroups.js
+++ backend/controllers/RoleGroups.js
@@ -1,7 +1,11 @@
 import RoleGroup from "../models/RoleGroup.js"
 export const getRole = async (req, res) => {
     try {
-        const rolegroups = await RoleGroup.findAll();
+        //const rolegroups = await RoleGroup.findAll();
+
+
+        const rolegroups = await RoleGroup.sequelize.query('select * from role_group');
+
         res.json(rolegroups);
     } catch (error) {
         res.json({ message: error.message });
