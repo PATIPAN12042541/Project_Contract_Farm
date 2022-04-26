02c94c61263751c7dd90424ea78af184d5c53831
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-26 08:55:24 +0700

    c

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -2,6 +2,9 @@ import Plant from "../models/Plant_M.js";
 
 export const getPlant = async (req, res) => {
   try {
+    
+    Plant.belongsTo(PlantDetail, { foreignKey: "id_plant" });
+
     const plant = await Plant.findAll();
     res.json(plant);
   } catch (error) {
--- backend/models/Plant_M.js
+++ backend/models/Plant_M.js
@@ -3,60 +3,31 @@ import db from "../config/Database.js";
 const { DataTypes } = Sequelize;
 
 const Plant = db.define(
-    "plant",
-    {
-      id_plant: {
-        type: DataTypes.INTEGER,
-        foreignKey: true,
-      },
-      name_plant: {
-        type: DataTypes.STRING,
-      },
-      start_date_plant: {
-        type: DataTypes.DATE,
-      },
-      end_date_plant: {
-        type: DataTypes.DATE,
-      },
-      plant_image: {
-        type: DataTypes.STRING,
-      },
+  "plant",
+  {
+    id_plant: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
     },
-    {
-      timestamps: false,
-      freezeTableName: true,
-    }
-  ),
-  PlantDetail = db.define(
-    "plant_detail",
-    {
-      id_plant: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-      },
-      id_name_plant: {
-        type: DataTypes.STRING,
-      },
-      quantity_chemical: {
-        type: DataTypes.INTEGER,
-      },
-      unit: {
-        type: DataTypes.STRING,
-      },
-      note: {
-        type: DataTypes.STRING,
-      },
-      last_update: {
-        type: DataTypes.DATE,
-      },
+    name_plant: {
+      type: DataTypes.STRING,
     },
-    {
-      timestamps: false,
-      freezeTableName: true,
-    }
-  );
+    start_date_plant: {
+      type: DataTypes.DATE,
+    },
+    end_date_plant: {
+      type: DataTypes.DATE,
+    },
+    plant_image: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
 
-Plant.belongsTo(PlantDetail, { foreignKey: "id_plant" });
 
 (async () => {
   await db.sync();
