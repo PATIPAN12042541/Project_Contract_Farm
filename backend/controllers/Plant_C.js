e725b00afd5c35be616b773132281e4c0609be0b
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-26 09:13:46 +0700

    c

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -1,13 +1,8 @@
-import Plant from "../models/Plant_M.js";
-import PlantDetail from "../models/PlantDetail_M.js";
+import Plants from "../models/Plant_M.js";
 
 export const getPlant = async (req, res) => {
   try {
-    Plant.hasMany(PlantDetail);
-    PlantDetail.belongsTo(Plant, { foreignKey: "id_plant" });
-
-    const plant = await Plant.findAll({ include: PlantDetail });
-    res.json(plant);
+    res.json(Plants);
   } catch (error) {
     res.json({ message: error.message });
   }
--- backend/models/Plant_M.js
+++ backend/models/Plant_M.js
@@ -3,34 +3,66 @@ import db from "../config/Database.js";
 const { DataTypes } = Sequelize;
 
 const Plant = db.define(
-  "plant",
-  {
-    id_plant: {
-      type: DataTypes.INTEGER,
-      foreignKey: true,
+    "plant",
+    {
+      id_plant: {
+        type: DataTypes.INTEGER,
+        foreignKey: true,
+      },
+      name_plant: {
+        type: DataTypes.STRING,
+      },
+      start_date_plant: {
+        type: DataTypes.DATE,
+      },
+      end_date_plant: {
+        type: DataTypes.DATE,
+      },
+      plant_image: {
+        type: DataTypes.STRING,
+      },
     },
-    name_plant: {
-      type: DataTypes.STRING,
+    {
+      timestamps: false,
+      freezeTableName: true,
+    }
+  ),
+  PlantDetail = db.define(
+    "plant_detail",
+    {
+      id_plant: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+      },
+      id_name_plant: {
+        type: DataTypes.STRING,
+      },
+      quantity_chemical: {
+        type: DataTypes.INTEGER,
+      },
+      unit: {
+        type: DataTypes.STRING,
+      },
+      note: {
+        type: DataTypes.STRING,
+      },
+      last_update: {
+        type: DataTypes.DATE,
+      },
     },
-    start_date_plant: {
-      type: DataTypes.DATE,
-    },
-    end_date_plant: {
-      type: DataTypes.DATE,
-    },
-    plant_image: {
-      type: DataTypes.STRING,
-    },
-  },
-  {
-    timestamps: false,
-    freezeTableName: true,
-  }
-);
+    {
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
 
+Plant.hasMany(PlantDetail, { primaryKey: "id_plant" });
+PlantDetail.belongsTo(Plant, { foreignKey: "id_plant" });
+
+const Plants = await Plant.findAll({ include: PlantDetail });
 
 (async () => {
   await db.sync();
 })();
 
-export default Plant;
+export default Plants;
