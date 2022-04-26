89ee467637b0d0979e88fd23a1e1bcc742284a0f
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-26 15:25:55 +0700

    c

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -4,21 +4,21 @@ import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
 
 export const getPlant = async (req, res) => {
+  //const plant = await Plant.findAll({ include: PlantDetail });
+  // const [results, metadata] = await Sequelize.query(
+  //   "select * from plant left join plant_detail on plant.id_plant = plant_detail.id"
+  // );
+  // "select * from plant left join plant_detail on plant.id_plant = plant_detail.id",
+
+  //const plant = await db.query("select * from plant");
   try {
-    /*const plant = await PlantDetail.findAll({
-      subQuery: false,
-      include: PlantDetail,
-    });*/
+    const data = {};
+    data.PlantDetail.hasMany(data.Plant, {
+      foreignKey: { name: "id_plant", field: "id_plant" },
+    });
 
-    //const plant = await Plant.findAll({ include: PlantDetail });
-    // const [results, metadata] = await Sequelize.query(
-    //   "select * from plant left join plant_detail on plant.id_plant = plant_detail.id"
-    // );
-    // "select * from plant left join plant_detail on plant.id_plant = plant_detail.id",
-
-    const plant = await db.query("select * from plant");
-
-    res.json(plant);
+    data.Plant.belongsTo(data.PlantDetail, { foreignKey: "id_plant" });
+    res.json(data);
   } catch (error) {
     res.json({ message: error.message });
   }
