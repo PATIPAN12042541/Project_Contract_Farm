77b226155f09c89a7f77345251d299b4883907ca
kobkij <Kobkij@globofoods.com>
2022-04-26 14:28:40 +0700

    commit

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -19,17 +19,10 @@ export const getPlant = async (req, res) => {
     });*/
 
     //const plant = await Plant.findAll({ include: PlantDetail });
-<<<<<<< HEAD
 
-    const plant = await Plant.sequelize.query('select * from plant left join plant_detail on plant.id_plant = plant_detail.id');
-
-    //const plant = await db.query('select * from plant left join plant_detail on plant.id_plant = plant_detail.id');
-=======
-    const plant = await Plant.sequelize.query(
-      "select * from plant " +
-        "left join plant_detail on plant.id_plant = plant_detail.id"
-    );
->>>>>>> 7121e2b7821aea8affd486df306d1b6ca4a55d68
+    //const plant = await Plant.sequelize.query('select * from plant left join plant_detail on plant.id_plant = plant_detail.id');
+     
+    const plant = await db.query('select * from plant left join plant_detail on plant.id_plant = plant_detail.id');
 
     res.json(plant);
   } catch (error) {
