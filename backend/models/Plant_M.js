2734c15749574968edd42c065c562ee6e856b9f0
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-26 14:11:42 +0700

    c

--- backend/models/Plant_M.js
+++ backend/models/Plant_M.js
@@ -3,7 +3,35 @@ import db from "../config/Database.js";
 import PlantDetail from "../models/PlantDetail_M.js";
 
 const { DataTypes } = Sequelize;
-const Plant = db.define("plant", {});
+const Plant = db.define(
+  "plant",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    id_plant: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    name_plant: {
+      type: DataTypes.STRING,
+    },
+    start_date_plant: {
+      type: DataTypes.DATE,
+    },
+    end_date_plant: {
+      type: DataTypes.DATE,
+    },
+    plant_image: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
 
 
 (async () => {
