7fa79e8fcbb8361c8f141ed0425d621f4657cd6f
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-26 15:35:17 +0700

    c

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -1,5 +1,7 @@
 import Plant from "../models/Plant_M.js";
 import PlantDetail from "../models/PlantDetail_M.js";
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
 
 export const getPlant = async (req, res) => {
   //const plant = await Plant.findAll({ include: PlantDetail });
@@ -10,14 +12,11 @@ export const getPlant = async (req, res) => {
 
   //const plant = await db.query("select * from plant");
   try {
-    const data = {};
-    data.PlantDetail.hasMany(data.Plant, {
-      foreignKey: "id_plants",
-    });
+    const plants = await db.query(
+      "select * from plant left join plant_detail on plant.id_plant = plant_detail.id"
+    );
 
-    data.Plant.belongsTo(data.PlantDetail, { foreignKey: "id_plant" });
-
-    res.json(data);
+    res.json(plants);
   } catch (error) {
     res.json({ message: error.message });
   }
