8fc7195f26ff17eb7e9ee2c309201e634863df32
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-26 15:55:16 +0700

    c

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -12,9 +12,9 @@ export const getPlant = async (req, res) => {
 
   //const plant = await db.query("select * from plant");
   try {
-    const plants = await db.query([
-      "select * from plant left join plant_detail on plant.id_plant = plant_detail.id_plant",
-    ]);
+    const plants = await db.query(
+      "select * from plant left join plant_detail on plant.id_plant = plant_detail.id_plant"
+    );
 
     res.json(plants);
   } catch (error) {
