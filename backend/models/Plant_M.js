c388e2b4887d1877eb9dd474119db75205a3bec2
kobkij <Kobkij@globofoods.com>
2022-04-26 11:16:44 +0700

    commit

--- backend/models/Plant_M.js
+++ backend/models/Plant_M.js
@@ -1,5 +1,6 @@
 import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
+import PlantDetail from "../models/PlantDetail_M.js";
 
 const { DataTypes } = Sequelize;
 const Plant = db.define(
