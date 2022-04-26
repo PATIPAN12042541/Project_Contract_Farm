1de94d61f9128a6b1f490fbdf95fc664d00d63a0
kobkij <Kobkij@globofoods.com>
2022-04-26 13:13:32 +0700

    commit

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -2,14 +2,20 @@ import Plant from "../models/Plant_M.js";
 import PlantDetail from "../models/PlantDetail_M.js";
 
 export const getPlant = async (req, res) => {
+  /*PlantDetail.hasMany(Plant);
+  PlantDetail.belongsTo(Plant);*/
+
   PlantDetail.hasMany(Plant);
-  PlantDetail.belongsTo(Plant);
+  Plant.belongsTo(PlantDetail);
 
   try {
-    const plant = await Plant.findAll({
+    /*const plant = await PlantDetail.findAll({
       subQuery: false,
       include: PlantDetail,
-    });
+    });*/
+
+    const plant = await Plant.findAll({ include: PlantDetail });
+
     res.json(plant);
   } catch (error) {
     res.json({ message: error.message });
