44795b0a33c9d02516464c09b2d31b2adeb65773
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-26 10:51:02 +0700

    c

--- backend/controllers/Plant_C.js
+++ backend/controllers/Plant_C.js
@@ -1,12 +1,14 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
 import Plant from "../models/Plant_M.js";
 import PlantDetail from "../models/PlantDetail_M.js";
 
 export const getPlant = async (req, res) => {
-
-  Plant.hasOne(PlantDetail);
+  Plant.hasMany(PlantDetail);
   PlantDetail.belongsTo(Plant);
+
   try {
-    const plant = await Plant.findAll({ PlantDetail });
+    const plant = await Plant.findAll({ include: PlantDetail });
     res.json(plant);
   } catch (error) {
     res.json({ message: error.message });
