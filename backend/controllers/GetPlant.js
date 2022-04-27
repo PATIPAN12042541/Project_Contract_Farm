57682628b6c64433ddf55c43d0657b19c3378b05
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-27 13:34:51 +0700

    c

--- backend/controllers/GetPlant.js
+++ backend/controllers/GetPlant.js
@@ -20,15 +20,15 @@ export const postPlant = async (req, res) => {
   const { id_name_plant, name_plant, start_date_plant, end_date_plant } =
     req.body;
 
-  // const IdPlant = await db.query(
-  //   "select id from plant_detail where id_name_plant = :id_name_plant ",
-  //   {
-  //     replacements: { id_name_plant: id_name_plant },
-  //     type: db.QueryTypes.SELECT,
-  //   }
-  // );
+  const IdPlant = await db.query(
+    "select id from plant_detail where id_name_plant = :id_name_plant ",
+    {
+      replacements: { id_name_plant: id_name_plant },
+      type: db.QueryTypes.SELECT,
+    }
+  );
 
-  // res.json(IdPlant);
+  res.json(IdPlant);
 
   try {
     await Plant.create({
