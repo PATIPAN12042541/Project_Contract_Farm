997f02b591d19deb39bf4c8768d22925802ed454
kobkij <Kobkij@globofoods.com>
2022-04-27 10:34:41 +0700

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -69,9 +69,9 @@ const Header = () => {
           </a>
           <div className="dropdown-menu dropdown-menu-lg dropdown-menu-right">
             <div className="dropdown-divider" />
-            <a href="#" onClick={Logout} className="dropdown-item">
+            <Link onClick={Logout} className="dropdown-item">
               <i className="fas fa-sign-out-alt mr-2" /> ออกจากระบบ
-            </a>
+            </Link>
             <div className="dropdown-divider" />
           </div>
         </li>
