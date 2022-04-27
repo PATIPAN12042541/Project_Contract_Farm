53fa6d60bcf6e5b757f7d32960254228ebaccd0c
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-27 14:29:19 +0700

    c

--- backend/routes/PlantRoute.js
+++ backend/routes/PlantRoute.js
@@ -1,14 +1,13 @@
 import express from "express";
 import {
   getPlant,
-  postPlant,
   postDetailPlant,
 } from "../controllers/GetPlant.js";
 
 const router = express.Router();
 
 router.get("/", getPlant);
-router.post("/Plant", postPlant);
+// router.post("/Plant", postPlant);
 router.post("/DetailPlant", postDetailPlant);
 
 export default router;
