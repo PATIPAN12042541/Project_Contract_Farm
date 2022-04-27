11125ffcf56f923d618d11d1b19b19358bbabc24
kobkij <Kobkij@globofoods.com>
2022-04-27 08:35:50 +0700

    commit

--- src/components/SidebarAdmin.js
+++ src/components/SidebarAdmin.js
@@ -105,12 +105,12 @@ const SidebarAdmin = () => {
           >
             <li class="nav-item">
               <li class="nav-item">
-                <Link to="/contract_farm" class="nav-link">
+                <a href="#" class="nav-link">
                   <p>
                     ข้อมูลรายละเอียด
                     <i class="fas fa-angle-left right"></i>
                   </p>
-                </Link>
+                </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
                     <Link className="nav-link" to="/Home">
@@ -131,12 +131,12 @@ const SidebarAdmin = () => {
             </li>
             <li class="nav-item">
               <li class="nav-item">
-                <Link to="/contract_farm" class="nav-link">
+                <a href="#" class="nav-link">
                   <p>
                     จัดการข้อมูล
                     <i class="fas fa-angle-left right"></i>
                   </p>
-                </Link>
+                </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
                     <Link to="/Edit_data" class="nav-link">
@@ -149,18 +149,18 @@ const SidebarAdmin = () => {
             </li>
             <li class="nav-item">
               <li class="nav-item">
-                <Link to="/contract_farm" class="nav-link">
+                <a class="nav-link">
                   <p>
                     เพิ่มเติม
                     <i class="fas fa-angle-left right"></i>
                   </p>
-                </Link>
+                </a>
                 <ul class="nav nav-treeview">
                   <li class="nav-item">
-                    <Link class="nav-link">
+                    <a href="#" class="nav-link">
                       <i class="far fa-circle nav-icon"></i>
                       <p>อื่นๆ</p>
-                    </Link>
+                    </a>
                   </li>
                 </ul>
               </li>
