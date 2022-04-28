63fff76e994ba06afb5ce90810dd9e96f7a2fdb7
kobkij <Kobkij@globofoods.com>
2022-04-28 13:43:07 +0700

    commit

--- src/components/Manage_data/Edit_data.js
+++ src/components/Manage_data/Edit_data.js
@@ -95,12 +95,12 @@ const Edit_data = () => {
           });
         });
 
-        let formData = new FormData()
+        /*let formData = new FormData()
         formData.append('file', image.data)
 
         await axios.post(`${process.env.REACT_APP_API_URL}/public/dist/img/`, formData)
                     .then(res => console.log(res.data))
-                    .catch(err => console.error(err));
+                    .catch(err => console.error(err));*/
     } catch (error) {
       Swal.fire({
         icon: "error",
