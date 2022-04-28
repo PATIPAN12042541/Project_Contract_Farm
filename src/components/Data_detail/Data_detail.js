6ae04310953bc02dd42b68e5cc999a939b7c0a5c
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-28 17:18:30 +0700

    c

--- src/components/Data_detail/Data_detail.js
+++ src/components/Data_detail/Data_detail.js
@@ -45,20 +45,18 @@ const Data_detail = (props) => {
                 <div className="card-body">
                   <div className="row">
                     <div className="col-12 col-sm-4">
-                      {datadetail.map((data) => (
-                        <div className="col-12">
-                          <SimpleImageSlider
-                            width={320}
-                            height={350}
-                            images={data}
-                            showBullets={true}
-                            showNavs={true}
-                            style={{ backgroundColor: "#FFFFF" }}
-                            autoPlay={true}
-                            className="product-image"
-                          />
-                        </div>
-                      ))}
+                      <div className="col-12">
+                        <SimpleImageSlider
+                          width={320}
+                          height={350}
+                          images={datadetail}
+                          showBullets={true}
+                          showNavs={true}
+                          style={{ backgroundColor: "#FFFFF" }}
+                          autoPlay={true}
+                          className="product-image"
+                        />
+                      </div>
                     </div>
                     <div className="col-12 col-sm-8">
                       <div className="col-12 col-md-12 col-lg-12 order-2 order-md-1">
