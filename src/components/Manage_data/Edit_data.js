653b53d44a95cfd82ceebc47a91a71e3a20eb39a
kobkij <Kobkij@globofoods.com>
2022-04-28 16:18:44 +0700

    commit

--- src/components/Manage_data/Edit_data.js
+++ src/components/Manage_data/Edit_data.js
@@ -214,6 +214,7 @@ const Edit_data = () => {
                                       data: file[0],
                                     };
                                     setImage(img);
+                                    console.log("query file 2", Image.name);
                                   }}
                                 />
                               </div>
