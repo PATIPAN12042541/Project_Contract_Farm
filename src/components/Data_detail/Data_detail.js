680396487d9d1b6c5663ddefc3227107f005a78c
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-28 16:42:25 +0700

    c

--- src/components/Data_detail/Data_detail.js
+++ src/components/Data_detail/Data_detail.js
@@ -7,6 +7,7 @@ import axios from "axios";
 const Data_detail = (props) => {
   const [datadetail, setDatadetail] = useState([]);
 
+  console.log(datadetail);
   useEffect(() => {
     getDatadetail();
   }, []);
@@ -45,20 +46,18 @@ const Data_detail = (props) => {
                 <div className="card-body">
                   <div className="row">
                     <div className="col-12 col-sm-4">
-                      {datadetail.map((data, index) => (
-                        <div className="col-12">
-                          <SimpleImageSlider
-                            width={320}
-                            height={350}
-                            images={data.path_image}
-                            showBullets={true}
-                            showNavs={true}
-                            style={{ backgroundColor: "#FFFFF" }}
-                            autoPlay={true}
-                            className="product-image"
-                          />
-                        </div>
-                      ))}
+                      <div className="col-12">
+                        <SimpleImageSlider
+                          width={320}
+                          height={350}
+                          images={data.path_image}
+                          showBullets={true}
+                          showNavs={true}
+                          style={{ backgroundColor: "#FFFFF" }}
+                          autoPlay={true}
+                          className="product-image"
+                        />
+                      </div>
                     </div>
                     <div className="col-12 col-sm-8">
                       <div className="col-12 col-md-12 col-lg-12 order-2 order-md-1">
