2c2246cce7ce60ed41efaff9169eb65fd24d539d
kobkij <Kobkij@globofoods.com>
2022-04-28 17:10:59 +0700

    commit

--- src/components/Manage_data/Edit_data.js
+++ src/components/Manage_data/Edit_data.js
@@ -337,7 +337,7 @@ const Edit_data = () => {
                               <div className="col-2">
                                 <input
                                   type="text"
-                                  className="form-control disabled"
+                                  className="form-control"
                                   placeholder="# หมายเลข "
                                   value={data.id_name_plant}
                                 />
