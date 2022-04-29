94590b54c62018cca33997daebd46c490dbf516a
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-29 10:01:25 +0700

    c

--- src/components/Data_detail/Data_detail.js
+++ src/components/Data_detail/Data_detail.js
@@ -40,37 +40,46 @@ const Data_detail = (props) => {
                 >
                   <h1 className="card-title">รายละเอียดแปลง A1 </h1>
                 </div>
-
                 <div className="card-body">
                   <div className="row">
                     <div className="col-12 col-sm-4">
                       <div className="col-12">
-                        <SimpleImageSlider
-                          width={320}
-                          height={350}
-                          images={""}
-                          showBullets={true}
-                          showNavs={true}
-                          style={{ backgroundColor: "#FFFFF" }}
-                          autoPlay={true}
-                          className="product-image"
+                        <img
+                          className="card-img-top"
+                          src={`"../dist/img/holy_basil.jpg"?w=248&fit=crop&auto=format`}
+                          alt={"พรีวาทอน"}
+                          loading="lazy"
+                          width={200}
+                          height={250}
                         />
                       </div>
                     </div>
                     <div className="col-12 col-sm-8">
                       <div className="col-12 col-md-12 col-lg-12 order-2 order-md-1">
                         <div className="row">
-                          <div className="col-12 col-sm-4">
+                          <div className="col-12 col-sm-3">
                             <div className="info-box bg-light">
                               <div className="info-box-content">
-                                <span className="info-box-text text-center text-muted">
-                                  วันที่เริ่มต้น 20/07/2022
+                                <span className="info-box-text text-center text-muted"></span>
+                                <span className="info-box-number text-center text-muted mb-0">
+                                  พรีวาทอน
                                 </span>
-                                <span className="info-box-number text-center text-muted mb-0"></span>
                               </div>
                             </div>
                           </div>
-                          <div className="col-12 col-sm-4">
+                          <div className="col-12 col-sm-3">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-text text-center text-muted">
+                                  วันที่เริ่มต้น
+                                </span>
+                                <span className="info-box-number text-center text-muted mb-0">
+                                  20/07/2022
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-12 col-sm-3">
                             <div className="info-box bg-light">
                               <div className="info-box-content">
                                 <span className="info-box-text text-center text-muted">
@@ -82,7 +91,7 @@ const Data_detail = (props) => {
                               </div>
                             </div>
                           </div>
-                          <div className="col-12 col-sm-4">
+                          <div className="col-12 col-sm-3">
                             <div className="info-box bg-light">
                               <div className="info-box-content">
                                 <span className="info-box-text text-center text-muted">
