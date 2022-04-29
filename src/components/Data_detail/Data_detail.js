63ccc969290fc1152028fc4d2e3644e4e67e994e
PATIPAN12042541 <patiphan@lobo.co.th>
2022-04-29 10:06:46 +0700

    c

--- src/components/Data_detail/Data_detail.js
+++ src/components/Data_detail/Data_detail.js
@@ -111,6 +111,89 @@ const Data_detail = (props) => {
               </div>
             </div>
           </div>
+
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card card-info">
+                <div
+                  className="card-header"
+                  style={{ backgroundColor: "#8CC152" }}
+                >
+                  <h1 className="card-title">รายละเอียดแปลง A1 </h1>
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-12 col-sm-4">
+                      <div className="col-12">
+                        <img
+                          className="card-img-top"
+                          src={"../dist/img/insecticide/Pic_1.png"}
+                          alt={"พรีวาทอน"}
+                          loading="lazy"
+                          width={200}
+                          height={250}
+                        />
+                        <ImageListItemBar
+                          className="card-title text-primary text-white"
+                          title={"พรีวาทอน"}
+                        />
+                      </div>
+                    </div>
+                    <div className="col-12 col-sm-8">
+                      <div className="col-12 col-md-12 col-lg-12 order-2 order-md-1">
+                        <div className="row">
+                          <div className="col-12 col-sm-4">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-text text-center text-muted">
+                                  วันที่เริ่มต้น
+                                </span>
+                                <span className="info-box-number text-center text-muted mb-0">
+                                  20/07/2022
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-12 col-sm-4">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-text text-center text-muted">
+                                  วันที่สิ้นสุด
+                                </span>
+                                <span className="info-box-number text-center text-muted mb-0">
+                                  20/07/2022
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-12 col-sm-4">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-text text-center text-muted">
+                                  ปริมาณสารเคมีที่ใช้
+                                </span>
+                                <span className="info-box-number text-center text-muted mb-0">
+                                  5 ml.
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-12">
+                            <h4>Note.</h4>
+                            <div className="post">
+                              <p>ยังอยู่ในช่วงทดสอบระบบนะครับ</p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
