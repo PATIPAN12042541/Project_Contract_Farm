033f79f754e24159af16b18722fd80696a1ec934
kobkij <Kobkij@globofoods.com>
2022-05-01 20:35:32 +0700

    commit

--- backend/controllers/Menus.js
+++ backend/controllers/Menus.js
@@ -1,12 +1,22 @@
 import Menus from "../models/RoleMenu.js";
 export const getMenusRoleMain = async (req, res) => {
     try {
-        const menus = await Menus.findAll({
-            where:{
-                role_id : req.params.role_id,
-                parent_id : "0"
+        const menus = await Menus.sequelize.query(
+            "SELECT id,"+
+            "       menu_name,"+
+            "       index_menu,"+
+            "       parent_id,"+
+            "       link,"+
+            "       role_id "+
+            "FROM role_menu "+
+            "where role_id = :role_id "+
+            "and status = 1 "+
+            "order by index_menu asc",
+            {
+                replacements: { role_id: req.params.role_id },
+                type: QueryTypes.SELECT
             }
-        });
+        );
         res.json(menus);
     } catch (error) {
         res.json({ message: error.message });
