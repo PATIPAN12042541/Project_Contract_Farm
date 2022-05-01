883599490c79c015370a7bdb80ceacf4a4d4c6bf
kobkij <Kobkij@globofoods.com>
2022-05-02 01:20:08 +0700

    commit

--- src/components/SidebarRole/SidebarRole.js
+++ src/components/SidebarRole/SidebarRole.js
@@ -48,8 +48,10 @@ const SidebarRole = () => {
     }
 
     const menusublv1 = async(role_id,parentid) => {
-        const menu_lv1 = await axios.get(`${process.env.REACT_APP_API_URL}/menu/sublv1/${role_id}/${parentid}`);
-        setSubMenuLV1(menu_lv1.data);
+        /*const menu_lv1 = await axios.get(`${process.env.REACT_APP_API_URL}/menu/sublv1/${role_id}/${parentid}`);
+        setSubMenuLV1(menu_lv1.data);*/
+        alert(role_id);
+        alert(parentid);
     }
 
     const axiosJWT = axios.create();
@@ -131,7 +133,8 @@ const SidebarRole = () => {
                 {mainmenu.map((item,index)=>{
                     return (
                         <li className="nav-item" 
-                            key={index}>
+                            key={index}
+                            onSelect={menusublv1(item.role_id,item.id)}>
                             <Link to={item.link} className="nav-link">
                                 <p>
                                 {item.menu_name}
