7c1a93124d6ad1b21797d84403d500459c84b993
kobkij <Kobkij@globofoods.com>
2022-05-01 23:05:25 +0700

    commit

--- src/components/SidebarRole/SidebarRole.js
+++ src/components/SidebarRole/SidebarRole.js
@@ -44,7 +44,7 @@ const SidebarRole = () => {
         setMainMenu(menu.data);
 
         alert("role_id : "+decoded.role_id);
-        alert("id 1 : "+menu.data.id);
+        alert("id 1 : "+menu[0].data.id);
         alert("id 2 : "+menu[0].data.id);
         menusublv1(decoded.role_id,menu.data.id);
 
