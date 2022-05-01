189d15cb68c2fc1eccb0599773e875d24391dbda
kobkij <Kobkij@globofoods.com>
2022-05-02 01:35:29 +0700

    commit

--- src/components/SidebarRole/SidebarRole.js
+++ src/components/SidebarRole/SidebarRole.js
@@ -134,8 +134,7 @@ const SidebarRole = () => {
                 {mainmenu.map((item,index)=>{
                     return (
                         <li className="nav-item" 
-                            key={index}
-                            onClick={menusublv1(item.role_id,item.id)}>
+                            key={index}>
                             <Link to={item.link} className="nav-link">
                                 <p>
                                 {item.menu_name}
