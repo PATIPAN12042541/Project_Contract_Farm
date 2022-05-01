c9b721303dc6f6a547557bf3b969ade12351bd7b
kobkij <Kobkij@globofoods.com>
2022-05-01 22:13:50 +0700

    commit

--- backend/controllers/Menus.js
+++ backend/controllers/Menus.js
@@ -1,14 +1,13 @@
 import Menus from "../models/RoleMenu.js";
-import db from "../config/Database.js";
 export const getMenusRoleMain = async (req, res) => {
     try {
         const menus = await Menus.findAll({
             where:{
                 role_id:req.params.id,
-                parent_id:0,
                 status:1,
             },
             order: [
+                ['parent_id','asc'],
                 ['index_menu','asc'],
             ]
         })
@@ -16,26 +15,4 @@ export const getMenusRoleMain = async (req, res) => {
     } catch (error) {
         res.json({ message: error.message });
     } 
-    
-    
-    /*try {
-        const menus = await db.query(
-            "SELECT id,"+
-            "       menu_name,"+
-            "       index_menu,"+
-            "       parent_id,"+
-            "       link,"+
-            "       role_id "+
-            "FROM role_menu "+
-            "where role_id = :role_id "+
-            "order by index_menu asc",
-            {
-                replacements: { role_id: req.params.id },
-                type: db.QueryTypes.SELECT
-            }
-        );
-        res.json(menus);
-    } catch (error) {
-        res.json({ message: error.message });
-    }  */
 }
