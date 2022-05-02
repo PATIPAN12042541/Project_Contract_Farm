6f68225eb56733d956ba368f2914af0104056182
kobkij <Kobkij@globofoods.com>
2022-05-02 15:21:12 +0700

    commit

--- src/components/SidebarRole/SidebarRole.js
+++ src/components/SidebarRole/SidebarRole.js
@@ -19,6 +19,8 @@ const SidebarRole = () => {
     useEffect(() => {
       refreshToken();
       //getUsers();
+      alert("name : "+{name});
+      alert("last_name : "+{last_name});
     }, []);
 
 
