4d4adf3acd30dc2551a44cb2717601d27fb4a1fc
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-04 10:53:18 +0700

    commit

--- src/components/Manage_data/Manage_plant_edit.js
+++ src/components/Manage_data/Manage_plant_edit.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Manage_plant_edit = () => {
-  return <div>แก้ไขข้อมูล Deatail</div>;
+  return <div className="content-wrapper">แก้ไขข้อมูล Deatail</div>;
 };
 
 export default Manage_plant_edit;
