78fecd2057ee44d935ad0ba2f75d7c9838a1e7e7
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-04 08:26:49 +0700

    commit

--- src/components/Manage_data/Manage_plant.js
+++ src/components/Manage_data/Manage_plant.js
@@ -60,7 +60,7 @@ const Manage_plant = (props) => {
     } catch (error) {
       Swal.fire({
         icon: "error",
-        title: "test",
+        title: "Error",
         text: "Save Error!",
       });
     }
