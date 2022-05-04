174fd13021732e349dd5b1f8975cd5afa3be3293
kobkij <Kobkij@globofoods.com>
2022-05-04 13:24:48 +0700

    commit

--- src/components/SidebarRole/SidebarRole.js
+++ src/components/SidebarRole/SidebarRole.js
@@ -55,28 +55,28 @@ const SidebarRole = () => {
 
     const axiosJWT = axios.create();
 
-    axiosJWT.interceptors.request.use(
-      async (config) => {
-        const currentDate = new Date();
-        if (expire * 1000 < currentDate.getTime()) {
-          const response = await axios.get(
-            `${process.env.REACT_APP_API_URL}/user/token`
-          );
-          //const response = await axios.get('http://localhost:4000/user/token');
-          config.headers.Authorization = `Bearer ${response.data.accessToken}`;
-          setToken(response.data.accessToken);
-          const decoded = jwt_decode(response.data.accessToken);
-          setName(decoded.name);
-          setLastName(decoded.last_name);
-          setRoleID(decoded.role_id);
-          setExpire(decoded.exp);
-        }
-        return config;
-      },
-      (error) => {
-        return Promise.reject(error);
-      }
-    );
+    // axiosJWT.interceptors.request.use(
+    //   async (config) => {
+    //     const currentDate = new Date();
+    //     if (expire * 1000 < currentDate.getTime()) {
+    //       const response = await axios.get(
+    //         `${process.env.REACT_APP_API_URL}/user/token`
+    //       );
+    //       //const response = await axios.get('http://localhost:4000/user/token');
+    //       config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+    //       setToken(response.data.accessToken);
+    //       const decoded = jwt_decode(response.data.accessToken);
+    //       setName(decoded.name);
+    //       setLastName(decoded.last_name);
+    //       setRoleID(decoded.role_id);
+    //       setExpire(decoded.exp);
+    //     }
+    //     return config;
+    //   },
+    //   (error) => {
+    //     return Promise.reject(error);
+    //   }
+    // );
 
     const getUsers = async () => {
       try{
