575af76c57881bd28384980ad09f86bf56b28fe2
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-05 15:22:26 +0700

    commit

--- backend/controllers/GetChemical.js
+++ backend/controllers/GetChemical.js
@@ -3,7 +3,7 @@ import db from "../config/Database.js";
 export const getChemical = async (req, res) => {
   try {
     const chemical = await db.query(
-      "SELECT name_chemical,' ( ',name_chemical_eng,' ) ')  FROM name_chemical",
+      "SELECT name_chemical,name_chemical_eng FROM name_chemical",
       {
         type: db.QueryTypes.SELECT,
       }
