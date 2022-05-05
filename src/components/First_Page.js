aa01c7ced11f4f5ccebe9bd179d8af5de38a738f
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-05 14:24:17 +0700

    commit

--- src/components/First_Page.js
+++ src/components/First_Page.js
@@ -1,6 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+function refreshPage() {
+  setTimeout(() => {
+    window.location.reload(false);
+  }, 500);
+}
 
 const First_Page = () => {
+  useEffect(() => {
+    refreshPage();
+  }, []);
 
   return (
     <div className="content-wrapper">
