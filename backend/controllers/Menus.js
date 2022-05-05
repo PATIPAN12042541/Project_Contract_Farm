f9b6f5332f85868d464af0a65571cac5daa75997
kobkij <Kobkij@globofoods.com>
2022-05-05 08:57:34 +0700

    commit

--- backend/controllers/Menus.js
+++ backend/controllers/Menus.js
@@ -27,7 +27,7 @@ export const getMenusRoleSubLV1 = async (req, res) => {
                 role_id:req.params.role_id,
                 //parent_id:req.params.parent_id,
                 parent_id : {
-                    [Op.gt]: 0
+                    [menus.gt]: 0
                 },
                 status:1
             },
