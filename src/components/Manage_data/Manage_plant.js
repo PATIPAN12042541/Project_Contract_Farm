caffe966085f8322aaa49306c73c97732682391f
kobkij <Kobkij@globofoods.com>
2022-05-08 00:38:42 +0700

    commit

--- src/components/Manage_data/Manage_plant.js
+++ src/components/Manage_data/Manage_plant.js
@@ -246,7 +246,7 @@ const Manage_plant = (props) => {
                                           className="img-fluid mb-2"
                                           width="100"
                                           height="100"
-                                          onLoad={console.log("image.preview : "+image.preview)}
+                                          onLoad={console.log("onLoad image.preview : "+image.preview)}
                                           onLoadStart={console.log("onLoadStart image.preview : "+image.preview)}
                                         />
                                       </Zoom>
