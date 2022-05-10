9d232eaab57f19902cccf977ce167e11dce75fba
kobkij <Kobkij@globofoods.com>
2022-05-10 08:34:26 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -7,21 +7,13 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
