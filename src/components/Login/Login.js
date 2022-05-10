a67edeb1c6a08fa36c50d42de105bbf5bf90e391
kobkij <Kobkij@globofoods.com>
2022-05-10 14:46:03 +0700

    commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -44,7 +44,7 @@ const Login = () => {
                                      className="form-control" 
                                      placeholder="Username"
                                      value={username} 
-                                     onChange={(e)=>setUsername(e.target.value)}
+                                     onChange={(e)=>this.setUsername(e.target.value)}
                                      />
                               <Form.Group className="input-group-append">
                                   <Form.Group className="input-group-text">
@@ -57,7 +57,7 @@ const Login = () => {
                                      className="form-control" 
                                      placeholder="Password" 
                                      value={password} 
-                                     onChange={(e)=>setPassword(e.target.value)} />
+                                     onChange={(e)=>this.setPassword(e.target.value)} />
                               <Form.Group className="input-group-append">
                                   <Form.Group className="input-group-text">
                                       <span className="fas fa-lock" />
