fdc515ce1aa4f38e2770d8965436f660cc896b2f
kobkij <Kobkij@globofoods.com>
2022-05-10 15:08:05 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-//import ReactDOM from 'react-dom';
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
+//import ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,20 +8,19 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
- const root = ReactDOM.createRoot(document.getElementById("root"));
- root.render(<App />);
+ //const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>
 // );
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
