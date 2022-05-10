a34451ca6dc5592c20fac0c7bc3b1bc4d9179b61
kobkij <Kobkij@globofoods.com>
2022-05-10 08:47:19 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,9 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
