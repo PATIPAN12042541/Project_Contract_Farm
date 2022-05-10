d3327010de30ed286592d9d00e01961e45d1b6dd
kobkij <Kobkij@globofoods.com>
2022-05-10 08:05:18 +0700

    commit

--- src/components/SidebarRole/SidebarRole.js
+++ src/components/SidebarRole/SidebarRole.js
@@ -6,6 +6,7 @@ import jwt_decode from "jwt-decode";
 import { useNavigate } from 'react-router-dom';
 import logo from "./logo/sprout.png";
 import user6 from "./logo/user6-128x128.jpg";
+import { ListItem } from '@mui/material';
 
 const SidebarRole = () => {
   const [name, setName] = useState("");
@@ -130,8 +131,8 @@ const SidebarRole = () => {
               role="menu"
               data-accordion="false"
             >
-              <li className="nav-item">
-                <li className="nav-item">
+              <ListItem className="nav-item">
+                <ListItem className="nav-item">
                   <a href={item.link} 
                      className="nav-link">
                     <p>
@@ -146,18 +147,18 @@ const SidebarRole = () => {
                     if (item.id === itemsublv1.parent_id) {
                       return (
                         <ul className="nav nav-treeview" key={index}>
-                          <li className="nav-item">
+                          <ListItem className="nav-item">
                             <Link className="nav-link" to={itemsublv1.link}>
                               <i className="far fa-circle nav-icon"></i>
                               <p>{itemsublv1.menu_name}</p>
                             </Link>
-                          </li>
+                          </ListItem>
                         </ul>
                       );
                     }
                   })}
-                </li>
-              </li>
+                </ListItem>
+              </ListItem>
             </ul>
           </nav>
         ))}
