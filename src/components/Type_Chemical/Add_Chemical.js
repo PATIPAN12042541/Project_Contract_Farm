29db1be1feebb7d0718f3695afc62b12fa9ed34f
kobkij <Kobkij@globofoods.com>
2022-05-11 16:37:03 +0700

    commit

--- src/components/Type_Chemical/Add_Chemical.js
+++ src/components/Type_Chemical/Add_Chemical.js
@@ -26,8 +26,8 @@ const Add_Chemical = () => {
                                   <div className="card-body">
                                       <div className="row">
                                           <div class="form-group row">
-                                              <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                                              <div class="col-sm-10">
+                                              <label for="inputEmail3" class="col-sm-3 col-form-label">Email</label>
+                                              <div class="col-sm-9">
                                                   <input type="email" class="form-control" id="inputEmail3" placeholder="Email" />
                                               </div>
                                           </div>
