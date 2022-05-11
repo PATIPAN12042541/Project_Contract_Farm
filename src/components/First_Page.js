da2f576169157a6a11b416704b568ba711892921
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-11 10:17:31 +0700

    commit

--- src/components/First_Page.js
+++ src/components/First_Page.js
@@ -3,19 +3,28 @@ import "./CSS/First_Page.css";
 
 const First_Page = () => {
   const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
+  const [long, setLong] = useState([]); 
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function (position) {
-      setLat(position.coords.latitude);
-      setLong(position.coords.longitude);
-    });
 
-    console.log("Latitude is:", lat);
-    console.log("Longitude is:", long);
-  }, [lat, long]);
+  lat = "14.8060348";
+  long = "100.030848";
 
-  return <div className="content-wrapper">ทดสอบ พยากรอากาศ</div>;
+  await fetch(`${process.env.BASE_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+ 
+  }, [lat,long])
+
+  return (
+  <div className="content-wrapper">
+    ทดสอบระบบ
+  </div>
+  );
 };
 
 export default First_Page;
