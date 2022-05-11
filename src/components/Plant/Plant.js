978837733ef4a848267fed9847205f17a9229be1
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-11 16:57:50 +0700

    commit

--- src/components/Plant/Plant.js
+++ src/components/Plant/Plant.js
@@ -1,7 +1,40 @@
 import React from "react";
 
 const Plant = (props) => {
-  return <div className="content-wrapper">ทดสอบ</div>;
+  return (
+    <div className="content-wrapper">
+      <section className="content-header">
+        <div className="container-fluid">
+          <div className="row mb-2">
+            <div className="col-sm-12"></div>
+          </div>
+        </div>
+      </section>
+      <section className="content">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card card-success">
+                <div
+                  className="card-header"
+                  style={{ backgroundColor: "#8CC152" }}
+                >
+                  <center>
+                    <h3 className="card-title">แปลงเพาะปลูก</h3>
+                  </center>
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                      
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Plant;
