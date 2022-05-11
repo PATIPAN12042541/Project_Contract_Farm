9f5bd1c9050cdf1f99fed51868ce05e8a9a2d1fa
kobkij <Kobkij@globofoods.com>
2022-05-11 16:41:19 +0700

    commit

--- src/components/Type_Chemical/Add_Chemical.js
+++ src/components/Type_Chemical/Add_Chemical.js
@@ -26,8 +26,8 @@ const Add_Chemical = () => {
                                   <div className="card-body">
                                       <div className="row">
                                           <div className="form-group row">
-                                              <Form.Label className="col-sm-3 col-form-label">ประเภทสารเคมี</Form.Label>
-                                              <div class="col-sm-9">
+                                              <Form.Label className="col-sm-4 col-form-label">ประเภทสารเคมี</Form.Label>
+                                              <div class="col-sm-8">
                                                   <Form.Control type="text" class="form-control" />
                                               </div>
                                           </div>
