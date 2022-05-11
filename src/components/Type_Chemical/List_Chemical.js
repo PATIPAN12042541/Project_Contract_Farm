463229085af452d5aa4b60f54bd174ccf6182800
kobkij <Kobkij@globofoods.com>
2022-05-11 15:42:50 +0700

    commit

--- src/components/Type_Chemical/List_Chemical.js
+++ src/components/Type_Chemical/List_Chemical.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const List_Chemical = () => {
   return (
-    <div>List_Chemical</div>
+      <div className="content-wrapper">
+          <section className="content-header">
+              <div className="container-fluid">
+                  <div className="row mb-2">
+                      <div className="col-sm-12"></div>
+                  </div>
+              </div>
+          </section>
+          <section className="content">
+              <div className="container-fluid">
+                  <div className="row">
+                      <div className="col-md-12">
+                          <div className="card card-success">
+                              <div className="card-body">
+                                  <div className="row">
+                                      
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
+      </div>
   )
 }
 
