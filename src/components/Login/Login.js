eb7c0658eec2114975917db288433fb4420cf962
kobkij <Kobkij@globofoods.com>
2022-05-11 07:47:56 +0700

    commit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -10,21 +10,6 @@ const Login = () => {
     const [msg, setMsg] = useState();
     const Nav = useNavigate();
 
-    (function(d, s, id) {
-        if (d.getElementById(id)) {
-            if (window.__TOMORROW__) {
-                window.__TOMORROW__.renderWidget();
-            }
-            return;
-        }
-        const fjs = d.getElementsByTagName(s)[0];
-        const js = d.createElement(s);
-        js.id = id;
-        js.src = "https://www.tomorrow.io/v1/widget/sdk/sdk.bundle.min.js";
-
-        fjs.parentNode.insertBefore(js, fjs);
-    })(document, 'script', 'tomorrow-sdk');
-
     const Auth = async (e) => {
         e.preventDefault();
         await axios.post(`${process.env.REACT_APP_API_URL}/user/login`, {
@@ -46,32 +31,6 @@ const Login = () => {
 
   
     return (
-      <>
-
-<div class="tomorrow"
-           data-location-id="106646"
-           data-language="TH"
-           data-unit-system="METRIC"
-           data-skin="light"
-           data-widget-type="aqiPollutant"
-           style="padding-bottom:22px;position:relative;"
-        >
-          <a
-            href="https://www.tomorrow.io/weather/"
-            rel="nofollow noopener noreferrer"
-            target="_blank"
-            style="position: absolute; bottom: 0; transform: translateX(-50%); left: 50%;"
-          >
-            <img
-              alt="Powered by Tomorrow.io"
-              src="https://weather-website-client.tomorrow.io/img/powered-by-tomorrow.svg"
-              width="140"
-              height="15"
-            />
-          </a>
-        </div>
-
-
       <div className="hold-transition login-page">
             <div className="login-box">
                 <div className="login-logo">
@@ -117,8 +76,6 @@ const Login = () => {
                 </div>
             </div>
         </div>
-        
-        </>
   )
 }
 
