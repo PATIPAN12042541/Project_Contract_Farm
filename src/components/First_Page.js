3d1380b99b4a3e093db0dc2200be090220ddfddd
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-11 10:49:52 +0700

    commit

--- src/components/First_Page.js
+++ src/components/First_Page.js
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
 import "./CSS/First_Page.css";
 
 const First_Page = () => {
-
   const [data, setData] = useState([]);
 
-  
   const get_api_weather = async () => {
     await fetch(
       `https://api.openweathermap.org/data/2.5//weather/?lat=14.8060348&lon=100.030848&units=metric&APPID=f95c293c45ca886ddb11fec556e1cb16`
@@ -22,11 +19,39 @@ const First_Page = () => {
     get_api_weather();
   }, []);
 
-
   return (
-  <div className="content-wrapper">
-    ทดสอบระบบ
-  </div>
+    <div className="content-wrapper">
+      <div
+        className={
+          typeof weather.main != "undefined"
+            ? weather.main.temp > 18
+              ? "App hot"
+              : "App cold"
+            : "App"
+        }
+      >
+        <main>
+          {typeof weather.main != "undefined" ? (
+            <div>
+              <div className="location-container">
+                <div className="location">
+                  {weather.name}, {weather.sys.country}
+                </div>
+                <div className="date"> {dateBuild(new Date())}</div>
+              </div>
+              <div className="weather-container">
+                <div className="temperature">
+                  {Math.round(weather.main.temp)}°C
+                </div>
+                <div className="weather">{weather.weather[0].main}</div>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </main>
+      </div>
+    </div>
   );
 };
 
