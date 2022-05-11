f69397b2de8967579c929f78e1f375cda277d4f0
kobkij <Kobkij@globofoods.com>
2022-05-11 16:05:37 +0700

    commit

--- src/components/Type_Chemical/Add_Chemical.js
+++ src/components/Type_Chemical/Add_Chemical.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Add_Chemical = () => {
   return (
-    <div>Add_Chemical</div>
+    <div className="content-wrapper">
+          <section className="content-header">
+              <div className="container-fluid">
+                  <div className="row mb-2">
+                      <div className="col-sm-12"></div>
+                  </div>
+              </div>
+          </section>
+          <section className="content">
+              <div className="container-fluid">
+                  <div className="row">
+                      <div className="col-md-12">
+                          <div className="card card-success">
+                              <div className="card-header"
+                                   style={{ backgroundColor: "#8CC152" }}>
+                                  <center>
+                                      <h3 className="card-title">เพิ่มประเภทข้อมูลสารเคมี</h3>
+                                  </center>
+                              </div>
+                              <div className="card-body">
+                                  <div className="row">
+                                      
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
+      </div>
   )
 }
 
