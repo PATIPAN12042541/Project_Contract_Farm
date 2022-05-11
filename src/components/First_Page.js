95225fe1839504c64332f1bd1faa11c7127f6f4e
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-11 10:25:05 +0700

    commit

--- src/components/First_Page.js
+++ src/components/First_Page.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import "./CSS/First_Page.css";
 
 const First_Page = () => {
@@ -6,16 +7,15 @@ const First_Page = () => {
   const [long, setLong] = useState([]); 
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-
-  await fetch(`${process.env.BASE_URL}/weather/?lat=14.8060348&lon=100.030848&units=metric&APPID=${process.env.API_KEY}`)
+  
+  await axios.get(`${process.env.BASE_URL}/weather/?lat=14.8060348&lon=100.030848&units=metric&APPID=${process.env.API_KEY}`)
       .then(res => res.json())
       .then(result => {
         setData(result)
         console.log(result);
       });
  
-  },[])
+
 
   return (
   <div className="content-wrapper">
