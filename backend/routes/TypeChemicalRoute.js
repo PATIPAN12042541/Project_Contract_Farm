5da78bcb18d9ebb726cfd1f948adea5f1c09170c
kobkij <Kobkij@globofoods.com>
2022-05-12 09:36:10 +0700

    commit

--- backend/routes/TypeChemicalRoute.js
+++ backend/routes/TypeChemicalRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {createTypeChemical} from "../controllers/TypeChemicals.js"
+import {createTypeChemical,getAllTypeChemical} from "../controllers/TypeChemicals.js"
 
 const router = express.Router();
 
 router.post("/addTypeChemical", createTypeChemical);
+router.get("/getTypeChemical", getAllTypeChemical);
 
 export default router;
