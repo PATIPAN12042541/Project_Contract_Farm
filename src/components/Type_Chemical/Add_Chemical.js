805ed24249e3c663227c893b351f9e4c9c75e5e5
kobkij <Kobkij@globofoods.com>
2022-05-13 11:12:01 +0700

    commit

--- src/components/Type_Chemical/Add_Chemical.js
+++ src/components/Type_Chemical/Add_Chemical.js
@@ -14,7 +14,7 @@ const Add_Chemical = () => {
         e.preventDefault();
         await axios.post(`${process.env.REACT_APP_API_URL}/chemical/addTypeChemical`,{
             type_chemical: typeChemical,
-            status : (checked)?"1":"0",
+            status : checkStatus,
         })
         .then(function (response) {
             Swal.fire({
@@ -73,11 +73,11 @@ const Add_Chemical = () => {
                                                     defaultChecked={false}
                                                     onChange={(e)=>{
                                                         setChecked(!checked);
-                                                        // if (checked === true){
-                                                        //     setCheckStatus("1");
-                                                        // }else{
-                                                        //     setCheckStatus("0");
-                                                        // }
+                                                        if (checked === true){
+                                                            setCheckStatus("1");
+                                                        }else{
+                                                            setCheckStatus("0");
+                                                        }
                                                     }}
                                                 />
                                             </div>
