a6ae88ad1a8a54d487c0badf20a0b5b593291e39
kobkij <Kobkij@globofoods.com>
2022-05-13 16:46:34 +0700

    commit

--- src/components/Type_Chemical/Update_Chemical.js
+++ src/components/Type_Chemical/Update_Chemical.js
@@ -82,6 +82,7 @@ const Update_Chemical = () => {
                                         <div className="form-group row">
                                           <Form.Label className="col-sm-2 col-form-label">Status</Form.Label>
                                             <div className="col-sm-10">
+                                                {checked}
                                                 <Form.Check
                                                     type="checkbox"
                                                     id="custom-switch"
