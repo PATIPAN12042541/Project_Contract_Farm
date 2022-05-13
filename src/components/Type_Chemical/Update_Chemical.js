0d05a6920919f0ff04d401d0fa70c65a967e6842
kobkij <Kobkij@globofoods.com>
2022-05-13 16:35:51 +0700

    commit

--- src/components/Type_Chemical/Update_Chemical.js
+++ src/components/Type_Chemical/Update_Chemical.js
@@ -8,7 +8,7 @@ import Swal from 'sweetalert2'
 
 const Update_Chemical = () => {
     const [typeChemical,setTypeChemical] = useState('');
-    const [checked, setChecked] = useState('');
+    const [checked, setChecked] = useState(false);
     //const [checkStatus, setCheckStatus] = useState('');
     const navigate = useNavigate();
     const {id} = useParams();
