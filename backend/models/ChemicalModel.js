6315089f42ff9342fd2697ea3213aed8faf26005
kobkij <Kobkij@globofoods.com>
2022-05-14 23:03:00 +0700

    commit

---
+++ backend/models/ChemicalModel.js
@@ -0,0 +1,25 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+const NameChemical = db.define('name_chemical',{
+    name_chemical:{
+        type: DataTypes.STRING,
+    },
+    name_chemical_eng:{
+        type: DataTypes.STRING,
+    },
+    eu_mrl:{
+        type: DataTypes.STRING,
+    },
+    path_img:{
+        type: DataTypes.STRING,
+    }
+},{
+    freezeTableName:true
+});
+ 
+(async () => {
+    await db.sync();
+})();
+ export default NameChemical;
