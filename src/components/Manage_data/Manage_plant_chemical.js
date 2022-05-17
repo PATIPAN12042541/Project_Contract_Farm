95d676dbe4968afad8de1753e1e1b8540ad4ac70
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-17 17:25:09 +0700

    commit

--- src/components/Manage_data/Manage_plant_chemical.js
+++ src/components/Manage_data/Manage_plant_chemical.js
@@ -48,18 +48,30 @@ const Manage_plant_chemical = (props) => {
           </div>
         </div>
       </div>
-      <div className="col-12 col-sm-2">
-        <div className="form-group">
-          <label>หน่วย</label>
-          <input type="text" className="form-control" />
-        </div>
-      </div>
-      <div className="col-12 col-sm-2">
-        <div className="form-group">
-          <label>หน่วย</label>
-          <input type="text" className="form-control" />
-        </div>
-      </div>
+      {getselect.map((data, index) => {
+        <div>
+          <div className="col-12 col-sm-2">
+            <div className="form-group">
+              <label>ชื่อไทย</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder={data.name_chemical}
+              />
+            </div>
+          </div>
+          <div className="col-12 col-sm-2">
+            <div className="form-group">
+              <label>ชื่ออังกฤษ</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder={data.name_chemical_eng}
+              />
+            </div>
+          </div>
+        </div>;
+      })}
     </div>
   );
 };
