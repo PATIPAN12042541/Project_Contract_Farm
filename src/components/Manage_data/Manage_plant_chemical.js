c2de8449ca6449937f8c85c540f2343fc8f01712
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-17 17:04:13 +0700

    commmit

--- src/components/Manage_data/Manage_plant_chemical.js
+++ src/components/Manage_data/Manage_plant_chemical.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-import Zoom from "react-medium-image-zoom";
+
 
 const Manage_plant_chemical = (props) => {
   const [getChemical, setGetChemical] = useState([]);
