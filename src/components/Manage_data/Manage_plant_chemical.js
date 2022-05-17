4824017d64b23b4e2ce5dcce80077793ee62f068
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-17 16:37:11 +0700

    commit

--- src/components/Manage_data/Manage_plant_chemical.js
+++ src/components/Manage_data/Manage_plant_chemical.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import Zoom from "react-medium-image-zoom";
 
 const Manage_plant_chemical = (props) => {
-  return <div className="content-wrapper">จัดการข้อมูล</div>;
+  const [getChemical, setGetChemical] = useState([]);
+
+  const getChemicals = async () => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/getChemical`
+    );
+    setGetChemical(response.data);
+  };
+
+  return (
+    <div className="content-wrapper">
+      จัดการข้อมูล
+      <div className="form-group">
+        <label>ชื่อสารเคมีที่ใช้</label>
+        <select className="form-control" placeholder="">
+          {getChemical.map((Chemical, index) => {
+            return <option key={index}>{Chemical.name_chemical}</option>;
+          })}
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default Manage_plant_chemical;
