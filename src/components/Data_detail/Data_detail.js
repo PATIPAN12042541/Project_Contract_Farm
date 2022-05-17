d025d2f582dcd03d1932985523eac80fe169a4b7
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-17 15:43:18 +0700

    commit

--- src/components/Data_detail/Data_detail.js
+++ src/components/Data_detail/Data_detail.js
@@ -29,102 +29,99 @@ const Data_detail = (props) => {
       </section>
       <section className="content">
         <div className="container-fluid">
-          {datadetail.map((data, index) => (
-            <div className="row" key={index}>
-              <div className="col-md-12">
-                <div className="card card-info">
-                  <div
-                    className="card-header"
-                    style={{ backgroundColor: "#8CC152" }}
-                  >
-                    <h1 className="card-title">
-                      รายละเอียดสารเคมี {data.name_chemical}
-                      {/* รายละเอียดสารเคมี {data.name_chemical} */}
-                    </h1>
-                  </div>
-                  <div className="card-body">
-                    <div className="row">
-                      <div className="col-12 col-sm-4">
-                        <div className="col-12">
-                          <img
-                            className="img-fluid"
-                            src={data.path_image}
-                            loading="lazy"
-                          />
-                        </div>
+          {/* {datadetail.map((data, index) => ( */}
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card card-info">
+                <div
+                  className="card-header"
+                  style={{ backgroundColor: "#8CC152" }}
+                >
+                  <h1 className="card-title">
+                    รายละเอียดสารเคมี
+                    {/* รายละเอียดสารเคมี {data.name_chemical} */}
+                  </h1>
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-12 col-sm-4">
+                      <div className="col-12">
+                        <img
+                          className="img-fluid"
+                          // src={data.path_image}
+                          loading="lazy"
+                        />
                       </div>
-                      <div className="col-12 col-sm-8">
-                        <div className="col-12 col-md-12 col-lg-12 order-2 order-md-1">
-                          <div className="row">
-                            <div className="col-12 col-sm-6">
-                              <div className="info-box bg-light">
-                                <div className="info-box-content">
-                                  <span className="info-box-text text-left text-black">
-                                    <b className="font-size2">ชื่อสามัญ : </b>
-                                    chlorantraniliprole
-                                  </span>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="col-12 col-sm-6">
-                              <div className="info-box bg-light">
-                                <div className="info-box-content">
-                                  <span className="info-box-text text-left text-black">
-                                    <b className="font-size2">
-                                      วัน/เดือน/ปี :{" "}
-                                    </b>
-                                    11/05/2565
-                                  </span>
-                                </div>
+                    </div>
+                    <div className="col-12 col-sm-8">
+                      <div className="col-12 col-md-12 col-lg-12 order-2 order-md-1">
+                        <div className="row">
+                          <div className="col-12 col-sm-6">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-text text-left text-black">
+                                  <b className="font-size2">ชื่อสามัญ : </b>
+                                  chlorantraniliprole
+                                </span>
                               </div>
                             </div>
                           </div>
-                          <div className="row">
-                            <div className="col-12 col-sm-6">
-                              <div className="info-box bg-light">
-                                <div className="info-box-content">
-                                  <span className="info-box-text text-left text-black">
-                                    <b className="font-size2">
-                                      อัตรา (cc : L.) :{" "}
-                                    </b>
-                                    40 cc : 25 L.
-                                  </span>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="col-12 col-sm-6">
-                              <div className="info-box bg-light">
-                                <div className="info-box-content">
-                                  <span className="info-box-text text-left text-black">
-                                    <b className="font-size2">
-                                      ระยะเวลาตกค้าง :{" "}
-                                    </b>
-                                    3 วัน
-                                  </span>
-                                </div>
+                          <div className="col-12 col-sm-6">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-text text-left text-black">
+                                  <b className="font-size2">วัน/เดือน/ปี : </b>
+                                  11/05/2565
+                                </span>
                               </div>
                             </div>
                           </div>
-                          <div className="row">
-                            <div className="col-12 col-sm-6">
-                              <div className="info-box bg-light">
-                                <div className="info-box-content">
-                                  <span className="info-box-number text-center text-black mb-0 font-size2">
-                                    วันที่เก็บเกี่ยวผลผลิตได้
-                                  </span>
-                                </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-12 col-sm-6">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-text text-left text-black">
+                                  <b className="font-size2">
+                                    อัตรา (cc : L.) :{" "}
+                                  </b>
+                                  40 cc : 25 L.
+                                </span>
                               </div>
-                            </div>
-                            <div className="col-12 col-sm-6 font-size">
-                              19/05/2565
                             </div>
                           </div>
-                          <div className="row">
-                            <div className="col-12">
-                              <h4>Note.</h4>
-                              <div className="post">
-                                <p>{data.note}</p>
+                          <div className="col-12 col-sm-6">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-text text-left text-black">
+                                  <b className="font-size2">
+                                    ระยะเวลาตกค้าง :{" "}
+                                  </b>
+                                  3 วัน
+                                </span>
                               </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-12 col-sm-6">
+                            <div className="info-box bg-light">
+                              <div className="info-box-content">
+                                <span className="info-box-number text-center text-black mb-0 font-size2">
+                                  วันที่เก็บเกี่ยวผลผลิตได้
+                                </span>
+                              </div>
+                            </div>
+                          </div>
+                          <div className="col-12 col-sm-6 font-size">
+                            19/05/2565
+                          </div>
+                        </div>
+                        <div className="row">
+                          <div className="col-12">
+                            <h4>Note.</h4>
+                            <div className="post">
+                              <p>-</p>
                             </div>
                           </div>
                         </div>
@@ -134,7 +131,8 @@ const Data_detail = (props) => {
                 </div>
               </div>
             </div>
-          ))}
+          </div>
+          {/* ))} */}
         </div>
       </section>
     </div>
