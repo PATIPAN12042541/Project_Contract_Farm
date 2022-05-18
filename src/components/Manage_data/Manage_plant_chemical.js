5771a503a1cd57036fdacc397a03064112971bb7
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-18 15:08:49 +0700

    commit

--- src/components/Manage_data/Manage_plant_chemical.js
+++ src/components/Manage_data/Manage_plant_chemical.js
@@ -123,21 +123,19 @@ const Manage_plant_chemical = (props) => {
                           value=""
                         />
                       </div>
-                      <div className="form-group row">
-                        <label className="col-sm-1 col-form-label">
-                          วันที่สิ้นสุด
-                        </label>
-                        <div
-                          className="col-sm-2 input-group date"
-                          data-target-input="nearest"
-                        >
-                          <input
-                            type="date"
-                            class="form-control form-control-border"
-                            placeholder="วันที่สิ้นสุด"
-                            value=""
-                          />
-                        </div>
+                      <label className="col-sm-1 col-form-label">
+                        วันที่สิ้นสุด
+                      </label>
+                      <div
+                        className="col-sm-2 input-group date"
+                        data-target-input="nearest"
+                      >
+                        <input
+                          type="date"
+                          class="form-control form-control-border"
+                          placeholder="วันที่สิ้นสุด"
+                          value=""
+                        />
                       </div>
                     </div>
                   </div>
