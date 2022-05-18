6fe324d8f44bceae87cfc4e380ab49b7a0ef4f22
kobkij <Kobkij@globofoods.com>
2022-05-18 12:02:05 +0700

    commit

--- src/components/Chemical/Update_Chemical.js
+++ src/components/Chemical/Update_Chemical.js
@@ -1,6 +1,6 @@
 import React, { useState ,useEffect} from 'react'
 import Form from 'react-bootstrap/Form'
-import { Link,useNavigate } from 'react-router-dom'
+import { Link,useNavigate,useParams } from 'react-router-dom'
 import axios from 'axios'
 import Zoom from "react-medium-image-zoom"
 import FileUpload from "@hawk-ui/file-upload"
