5895f9ee3de28ce1c66a72c8e9160b8ee7866988
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-18 16:32:14 +0700

    commit

--- backend/controllers/GetChemical.js
+++ backend/controllers/GetChemical.js
@@ -42,7 +42,7 @@ export const getChemicalByID = async (req, res) => {
 export const getSelect = async (req, res) => {
   try {
     const Select = await db.query(
-      "SELECT id,name_chemical,name_chemical_eng,eu_mrl,path_img FROM name_chemical where status = 1  id = :id  ",
+      "SELECT id,name_chemical,name_chemical_eng,eu_mrl,path_img FROM name_chemical where  id = :id  ",
       {
         replacements: { id: req.params.id },
         type: db.QueryTypes.SELECT,
