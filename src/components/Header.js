ad7455890de1270ef278f0bd7c3742364b6d53c8
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-20 14:40:36 +0700

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const history = useNavigate();
-  const [data, setData] = useState([]);
   const [checktime, setCheckTime] = useState([]);
   const [countchecktime, setCountCheckTime] = useState([]);
 
@@ -27,18 +26,7 @@ const Header = () => {
     setCountCheckTime(response.data.lenght);
   };
 
-  const get_api_weather = async () => {
-    await fetch(
-      `https://api.openweathermap.org/data/2.5//weather/?lat=14.8060348&lon=100.030848&units=metric&APPID=f95c293c45ca886ddb11fec556e1cb16`
-    )
-      .then((res) => res.json())
-      .then((result) => {
-        setData(result);
-      });
-  };
-
   useEffect(() => {
-    get_api_weather();
     getCheckTime();
   }, []);
 
