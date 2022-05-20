39385e6021cb3f9666787d436ad97a3cc14a527e
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-20 17:32:37 +0700

    commit

--- src/components/Plant/Plant.js
+++ src/components/Plant/Plant.js
@@ -12,7 +12,6 @@ const Plant = (props) => {
       `${process.env.REACT_APP_API_URL}/zoneplant/plant/${props.id}`
     );
     setDatadetail(response.data);
-    console.log(response.data);
   };
 
   useEffect(() => {
