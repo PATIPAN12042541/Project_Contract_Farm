7afa4b9c35d4561c96ee8165b4bde747fff76536
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-20 10:03:51 +0700

    commit

--- src/components/First_Page.js
+++ src/components/First_Page.js
@@ -93,53 +93,13 @@ const First_Page = () => {
                       </a>
                       <span className="users-list-date">Yesterday</span>
                     </li>
-                    <li>
-                      <img src="dist/img/user7-128x128.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        Jane
-                      </a>
-                      <span className="users-list-date">12 Jan</span>
-                    </li>
-                    <li>
-                      <img src="dist/img/user6-128x128.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        John
-                      </a>
-                      <span className="users-list-date">12 Jan</span>
-                    </li>
-                    <li>
-                      <img src="dist/img/user2-160x160.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        Alexander
-                      </a>
-                      <span className="users-list-date">13 Jan</span>
-                    </li>
-                    <li>
-                      <img src="dist/img/user5-128x128.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        Sarah
-                      </a>
-                      <span className="users-list-date">14 Jan</span>
-                    </li>
-                    <li>
-                      <img src="dist/img/user4-128x128.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        Nora
-                      </a>
-                      <span className="users-list-date">15 Jan</span>
-                    </li>
-                    <li>
-                      <img src="dist/img/user3-128x128.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        Nadia
-                      </a>
-                      <span className="users-list-date">15 Jan</span>
-                    </li>
                   </ul>
                 </div>
               </div>
-
-              <div class="card-footer text-center" style={{ display: "block" }}>
+              <div
+                className="card-footer text-center"
+                style={{ display: "block" }}
+              >
                 <a href="javascript:">View All Users</a>
               </div>
             </div>
