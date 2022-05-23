1e8b91fa5f279e34931e55947bed1e2573a28647
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-23 16:36:18 +0700

    commit

--- src/components/First_Page.js
+++ src/components/First_Page.js
@@ -7,8 +7,8 @@ const First_Page = () => {
   const [data, setData] = useState([]);
 
   const dateBuild = (d) => {
-    let date = String(new window.Date());
-    let date2 = moment(date).format("DD-MMM-YYYY");
+    //let date = String(new window.Date());
+    let date2 = moment(Date()).format("DD-MMM-YYYY");
 
     return date2;
   };
@@ -69,7 +69,6 @@ const First_Page = () => {
                       <div className="col-sm-10">
                         <div className="location-container">
                           <div className="location">
-                            {" "}
                             {data.name}, {data.sys.country}
                           </div>
                           <div className="date"> {dateBuild(new Date())}</div>
@@ -98,93 +97,8 @@ const First_Page = () => {
               </div>
             </div>
           </div>
-          {/* <div className="row">
-            <div className="col-12 col-sm-6 col-md-3">
-              <div className="card">
-                <div className="card-header">
-                  <h3 className="card-title">Latest Members</h3>
-                  <div className="card-tools">
-                    <button
-                      type="button"
-                      className="btn btn-tool"
-                      data-card-widget="collapse"
-                    >
-                      <i className="fas fa-minus"></i>
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-tool"
-                      data-card-widget="remove"
-                    >
-                      <i className="fas fa-times"></i>
-                    </button>
-                  </div>
-                </div>
-                <div className="card-body p-0" style={{ display: "block" }}>
-                  <ul className="users-list clearfix">
-                    <li>
-                      <img src="dist/img/user1-128x128.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        Alexander Pierce
-                      </a>
-                      <span className="users-list-date">Today</span>
-                    </li>
-                    <li>
-                      <img src="dist/img/user8-128x128.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        Norman
-                      </a>
-                      <span className="users-list-date">Yesterday</span>
-                    </li>
-                    <li>
-                      <img src="dist/img/user8-128x128.jpg" alt="User Image" />
-                      <a className="users-list-name" href="#">
-                        Norman
-                      </a>
-                      <span className="users-list-date">Yesterday</span>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div> */}
         </div>
       </section>
-      {/* <div
-        className={
-          typeof data.main != "undefined"
-            ? data.main.temp > 18
-              ? "App hot"
-              : "App cold"
-            : "App"
-        }
-      >
-        <main>
-          {typeof data.main != "undefined" ? (
-            <div>
-              <div className="location-container">
-                <div className="location">
-                  {data.name}, {data.sys.country}
-                </div>
-                <div className="date"> {dateBuild(new Date())}</div>
-              </div>
-              <div className="weather-container">
-                <div className="temperature">
-                  {Math.round(data.main.temp)}°C
-                </div>
-                <div className="weather">
-                  {data.weather[0].main}
-                  <img
-                    src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
-                  />
-                </div>
-              </div>
-            </div>
-          ) : (
-            ""
-          )}
-        </main>
-      </div> */}
     </div>
   );
 };
