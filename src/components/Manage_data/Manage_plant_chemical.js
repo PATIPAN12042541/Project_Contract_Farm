f38694b08389b67831da094e5e8788df203c0fe6
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-24 10:43:06 +0700

    commit

--- src/components/Manage_data/Manage_plant_chemical.js
+++ src/components/Manage_data/Manage_plant_chemical.js
@@ -361,12 +361,13 @@ const Manage_plant_chemical = (props) => {
               })}
               <div className="card-footer">
                 <button
-                  type="submit"
+                  type="button"
                   className="btn btn-default float-right"
                   style={{
                     backgroundColor: "#8CC152",
                     color: "#FFFFFF",
                   }}
+                  onClick={Checkdata}
                 >
                   ยืนยัน
                 </button>
