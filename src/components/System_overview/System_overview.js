1df67198fb07d02a1467328501bd87768a46959c
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-26 10:08:01 +0700

    commmit

--- src/components/System_overview/System_overview.js
+++ src/components/System_overview/System_overview.js
@@ -29,71 +29,69 @@ const System_overview = () => {
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-12">
-              <div className="card-header">
-                <h3 className="card-title">Fixed Header Table</h3>
-                <div className="card-tools">
-                  <div
-                    className="input-group input-group-sm"
-                  >
-                    <input
-                      type="text"
-                      className="form-control float-right"
-                      placeholder="Search"
-                    />
-                    <div className="input-group-append">
-                      <button type="submit" className="btn btn-default">
-                        <i className="fas fa-search"></i>
-                      </button>
+              <div className="card">
+                <div className="card-header">
+                  <h3 className="card-title">Fixed Header Table</h3>
+                  <div className="card-tools">
+                    <div className="input-group input-group-sm">
+                      <input
+                        type="text"
+                        className="form-control float-right"
+                        placeholder="Search"
+                      />
+                      <div className="input-group-append">
+                        <button type="submit" className="btn btn-default">
+                          <i className="fas fa-search"></i>
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              <div
-                className="card-body table-responsive p-0"
-              >
-                <table className="table table-head-fixed text-nowrap">
-                  <thead>
-                    <tr>
-                      <th>#</th>
-                      <th>ชื่อเเปลง</th>
-                      <th>วันที่เริ่มต้น</th>
-                      <th>วันที่สิ้นสุด</th>
-                      <th>ผู้รับผิดชอบ</th>
-                      <th>ชื่อสารเคมี/ปุ๋ย</th>
-                      <th>EU MRL</th>
-                      <th>ระยะเวลา</th>
-                      <th>ปริมาณที่ใช้</th>
-                      <th>Note</th>
-                      <th>วันที่เริ่มต้นสารเคมี</th>
-                      <th>วันที่สิ้นสุดสารเคมี</th>
-                      <th>สถานะ</th>
-                    </tr>
-                  </thead>
-                  {Overview.map((data, index) => (
-                    <tbody key={index}>
+                <div className="card-body table-responsive p-0">
+                  <table className="table table-head-fixed text-nowrap">
+                    <thead>
                       <tr>
-                        <td>{data.id_name_plant}</td>
-                        <td>{data.name_plant}</td>
-                        <td>{data.start_date_plant}</td>
-                        <td>{data.end_date_plant}</td>
-                        <td>{data.name + " " + data.last_name}</td>
-                        <td>
-                          {data.name_chemical +
-                            " ( " +
-                            data.name_chemical_eng +
-                            " ) "}
-                        </td>
-                        <td>{data.eu_mrl}</td>
-                        <td>{data.time + " " + data.unit}</td>
-                        <td>{data.cc + " CC / " + data.liter + " L"}</td>
-                        <td>{data.note}</td>
-                        <td>{data.date_start}</td>
-                        <td>{data.date_end}</td>
-                        <td>{data.status_check}</td>
+                        <th>#</th>
+                        <th>ชื่อเเปลง</th>
+                        <th>วันที่เริ่มต้น</th>
+                        <th>วันที่สิ้นสุด</th>
+                        <th>ผู้รับผิดชอบ</th>
+                        <th>ชื่อสารเคมี/ปุ๋ย</th>
+                        <th>EU MRL</th>
+                        <th>ระยะเวลา</th>
+                        <th>ปริมาณที่ใช้</th>
+                        <th>Note</th>
+                        <th>วันที่เริ่มต้นสารเคมี</th>
+                        <th>วันที่สิ้นสุดสารเคมี</th>
+                        <th>สถานะ</th>
                       </tr>
-                    </tbody>
-                  ))}
-                </table>
+                    </thead>
+                    {Overview.map((data, index) => (
+                      <tbody key={index}>
+                        <tr>
+                          <td>{data.id_name_plant}</td>
+                          <td>{data.name_plant}</td>
+                          <td>{data.start_date_plant}</td>
+                          <td>{data.end_date_plant}</td>
+                          <td>{data.name + " " + data.last_name}</td>
+                          <td>
+                            {data.name_chemical +
+                              " ( " +
+                              data.name_chemical_eng +
+                              " ) "}
+                          </td>
+                          <td>{data.eu_mrl}</td>
+                          <td>{data.time + " " + data.unit}</td>
+                          <td>{data.cc + " CC / " + data.liter + " L"}</td>
+                          <td>{data.note}</td>
+                          <td>{data.date_start}</td>
+                          <td>{data.date_end}</td>
+                          <td>{data.status_check}</td>
+                        </tr>
+                      </tbody>
+                    ))}
+                  </table>
+                </div>
               </div>
             </div>
           </div>
