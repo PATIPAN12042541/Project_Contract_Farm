6991b797b90fbd263dbf7c5414d6c357fcada6e5
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-27 16:42:55 +0700

    commit

--- src/components/System_overview/System_overview.js
+++ src/components/System_overview/System_overview.js
@@ -14,95 +14,99 @@ const System_overview = () => {
     console.log(overview.data);
   };
 
-  useEffect(() => {
-    getOverview();
-  }, []);
+const ExpandedComponent = ({ overview }) => (
+  <pre>{JSON.stringify(overview, null, 2)}</pre>
+);
+useEffect(() => {
+  getOverview();
+}, []);
 
-  const columns = [
-    {
-      name: "โซนเพาะปลูก",
-      selector: (row) => row.zone_id,
-      sortable: true,
-    },
-    {
-      name: "ชื่อเเปลง",
-      selector: (row) => row.name_plant,
-      sortable: true,
-    },
-    {
-      name: "วันที่เริ่มต้น",
-      selector: (row) => row.start_date_plant,
-      sortable: true,
-    },
-    {
-      name: "วันที่สิ้นสุด",
-      selector: (row) => row.end_date_plant,
-      sortable: true,
-    },
-    {
-      name: "ผู้รับผิดชอบ",
-      selector: (row) => row.name,
-      sortable: true,
-    },
-    {
-      name: "ชื่อสารเคมี/ปุ๋ย",
-      selector: (row) => row.name_chemical,
-      sortable: true,
-    },
-    {
-      name: "ระยะเวลา",
-      selector: (row) => row.days,
-      sortable: true,
-    },
-    {
-      name: "cc/L",
-      selector: (row) => row.quantity,
-      sortable: true,
-    },
-    {
-      name: "Note",
-      selector: (row) => row.note,
-      sortable: true,
-    },
-    {
-      name: "วันที่เริ่มต้นสารเคมี",
-      selector: (row) => row.date_start,
-      sortable: true,
-    },
-    {
-      name: "วันที่สิ้นสุดสารเคมี",
-      selector: (row) => row.date_end,
-      sortable: true,
-    },
-    {
-      name: "สถานะ",
-      selector: (row) => row.status_check,
-      sortable: true,
-    },
-  ];
+const columns = [
+  {
+    name: "โซนเพาะปลูก",
+    selector: (row) => row.zone_id,
+    sortable: true,
+  },
+  {
+    name: "ชื่อเเปลง",
+    selector: (row) => row.name_plant,
+    sortable: true,
+  },
+  {
+    name: "วันที่เริ่มต้น",
+    selector: (row) => row.start_date_plant,
+    sortable: true,
+  },
+  {
+    name: "วันที่สิ้นสุด",
+    selector: (row) => row.end_date_plant,
+    sortable: true,
+  },
+  {
+    name: "ผู้รับผิดชอบ",
+    selector: (row) => row.name,
+    sortable: true,
+  },
+  {
+    name: "ชื่อสารเคมี/ปุ๋ย",
+    selector: (row) => row.name_chemical,
+    sortable: true,
+  },
+  {
+    name: "ระยะเวลา",
+    selector: (row) => row.days,
+    sortable: true,
+  },
+  {
+    name: "cc/L",
+    selector: (row) => row.quantity,
+    sortable: true,
+  },
+  {
+    name: "Note",
+    selector: (row) => row.note,
+    sortable: true,
+  },
+  {
+    name: "วันที่เริ่มต้นสารเคมี",
+    selector: (row) => row.date_start,
+    sortable: true,
+  },
+  {
+    name: "วันที่สิ้นสุดสารเคมี",
+    selector: (row) => row.date_end,
+    sortable: true,
+  },
+  {
+    name: "สถานะ",
+    selector: (row) => row.status_check,
+    sortable: true,
+  },
+];
 
-  return (
-    <div className="content-wrapper">
-      <section className="content-header">
-        <div className="container-fluid">
-          <div className="row mb-2">
-            <div className="col-sm-12"></div>
-          </div>
+return (
+  <div className="content-wrapper">
+    <section className="content-header">
+      <div className="container-fluid">
+        <div className="row mb-2">
+          <div className="col-sm-12"></div>
         </div>
-      </section>
-      <section className="content">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-12">
-              <div className="card">
-                <div className="card-body table-responsive p-0">
-                  <DataTable
-                    title="ข้อมูลทั้งหมด"
-                    columns={columns}
-                    data={Overview}
-                    pagination
-                  />
-                  {/* <table className="table table-head-fixed text-nowrap">
+      </div>
+    </section>
+    <section className="content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body table-responsive p-0">
+                <DataTable
+                  title="ข้อมูลทั้งหมด"
+                  columns={columns}
+                  data={Overview}
+                  expandableRows
+                  expandableRowsComponent={ExpandedComponent}
+                />
+                {/* <table className="table table-head-fixed text-nowrap">
                     <thead>
                       <tr>
                         <th>โซนเพาะปลูก</th>
@@ -199,14 +203,14 @@ const System_overview = () => {
                       </tbody>
                     ))}
                   </table> */}
-                </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
-  );
+      </div>
+    </section>
+  </div>
+);
 };
 
 export default System_overview;
