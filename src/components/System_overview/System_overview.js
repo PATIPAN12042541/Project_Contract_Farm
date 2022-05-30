704054632bc36ad9a3142d44933802ad827880d5
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-30 14:52:43 +0700

    commit

--- src/components/System_overview/System_overview.js
+++ src/components/System_overview/System_overview.js
@@ -166,7 +166,7 @@ const System_overview = () => {
                 <div className="card-body">
                   <div className="row right">
                     <input
-                      className="input-css col-4"
+                      className="input-css col-8"
                       icon="search"
                       placeholder="Search..."
                       onChange={(e) => searchItems(e.target.value)}
