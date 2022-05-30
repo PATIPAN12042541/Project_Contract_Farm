24fa98d829a5e93ef3a21a4436f5597e9c1c35c5
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-30 08:54:30 +0700

    commit

--- backend/controllers/GetSystemOverview.js
+++ backend/controllers/GetSystemOverview.js
@@ -4,6 +4,11 @@ export const getSystemOverview = async (req, res) => {
   try {
     const Overview = await db.query(
       "SELECT " +
+        "CASE WHEN plant_detail.id_name_plant IS NULL THEN " +
+        "zone_plant.zone_name " +
+        "ELSE " +
+        "CONCAT(zone_plant.zone_name,'-',plant_detail.id_name_plant) " +
+        "END AS zone_id, " +
         "CONCAT(zone_plant.zone_name,'-',plant_detail.id_name_plant) AS zone_id," +
         "plant.name_plant," +
         "plant.start_date_plant," +
@@ -27,41 +32,6 @@ export const getSystemOverview = async (req, res) => {
         "LEFT JOIN plant_data_detail ON plant_detail.id = plant_data_detail.id_plant " +
         "LEFT JOIN name_chemical ON plant_data_detail.id_name_chemical = name_chemical.id " +
         "LEFT JOIN residual_period_chemical ON plant_data_detail.id_residual_period = residual_period_chemical.id ",
-
-      // "SELECT zone_plant.id as id_zone," +
-      //   "zone_plant.zone_name," +
-      //   "zone_plant.image_zone," +
-      //   "plant_detail.id as id_plant," +
-      //   "plant_detail.id_name_plant," +
-      //   "plant.name_plant," +
-      //   "plant.start_date_plant," +
-      //   "plant.end_date_plant," +
-      //   "plant.plant_image," +
-      //   "user.name," +
-      //   "user.last_name," +
-      //   "name_chemical.name_chemical," +
-      //   "name_chemical.name_chemical_eng," +
-      //   "name_chemical.eu_mrl," +
-      //   "name_chemical.path_img," +
-      //   "residual_period_chemical.time," +
-      //   "residual_period_chemical.unit," +
-      //   "plant_data_detail.cc," +
-      //   "plant_data_detail.liter," +
-      //   "plant_data_detail.note," +
-      //   "plant_data_detail.date_start," +
-      //   "plant_data_detail.date_end," +
-      //   "CASE " +
-      //   "WHEN plant_data_detail.status_check = 0 THEN 'Success' " +
-      //   "WHEN plant_data_detail.status_check IS NULL THEN 'Not Data Found' " +
-      //   "ELSE 'Not Success' " +
-      //   "END AS status_check " +
-      //   "FROM zone_plant " +
-      //   "LEFT JOIN plant_detail ON zone_plant.id = plant_detail.id_zone " +
-      //   "LEFT JOIN plant ON plant_detail.id = plant.id_plant " +
-      //   "LEFT JOIN user ON plant.id_user = user.id " +
-      //   "LEFT JOIN plant_data_detail ON plant_detail.id = plant_data_detail.id_plant " +
-      //   "LEFT JOIN name_chemical ON plant_data_detail.id_name_chemical = name_chemical.id " +
-      //   "LEFT JOIN residual_period_chemical ON plant_data_detail.id_residual_period = residual_period_chemical.id ",
       {
         replacements: { zone_plant_id: req.params.id },
         type: db.QueryTypes.SELECT,
--- src/components/System_overview/System_overview.js
+++ src/components/System_overview/System_overview.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import moment from "moment";
 import axios from "axios";
 import DataTable from "react-data-table-component";
 
@@ -18,6 +17,7 @@ const System_overview = () => {
     getOverview();
   }, []);
 
+  // header columns
   const columns = [
     {
       name: "โซนเพาะปลูก",
@@ -100,105 +100,8 @@ const System_overview = () => {
                     title="ข้อมูลทั้งหมด"
                     columns={columns}
                     data={Overview}
-                    pagination  
+                    pagination
                   />
-                  {/* <table className="table table-head-fixed text-nowrap">
-                    <thead>
-                      <tr>
-                        <th>โซนเพาะปลูก</th>
-                        <th>ชื่อเเปลง</th>
-                        <th>วันที่เริ่มต้น</th>
-                        <th>วันที่สิ้นสุด</th>
-                        <th>ผู้รับผิดชอบ</th>
-                        <th>ชื่อสารเคมี/ปุ๋ย</th>
-                        <th>ระยะเวลา</th>
-                        <th>ปริมาณที่ใช้</th>
-                        <th>Note</th>
-                        <th>วันที่เริ่มต้นสารเคมี</th>
-                        <th>วันที่สิ้นสุดสารเคมี</th>
-                        <th>
-                          <center>สถานะ</center>
-                        </th>
-                      </tr>
-                    </thead>
-                    {Overview.map((data, index) => (
-                      <tbody key={index}>
-                        <tr>
-                          <td>
-                            {data.zone_name == "null" ? "" : data.zone_name}
-                            {data.id_name_plant == "null" ? "" : " - "}
-                            {data.id_name_plant == "null"
-                              ? ""
-                              : data.id_name_plant}
-                          </td>
-                          <td>
-                            {data.name_plant == "null" ? "" : data.name_plant}
-                          </td>
-                          <td>
-                            {data.start_date_plant == "null"
-                              ? ""
-                              : data.start_date_plant}
-                          </td>
-                          <td>
-                            {data.end_date_plant == "null"
-                              ? ""
-                              : data.end_date_plant}
-                          </td>
-                          <td>
-                            {data.name == "null" ? "" : data.name}{" "}
-                            {data.last_name == "null" ? "" : data.last_name}
-                          </td>
-                          <td>
-                            {data.name_chemical == "null"
-                              ? ""
-                              : data.name_chemical}
-                            {data.name_chemical == "null" &&
-                            data.name_chemical_eng == "null"
-                              ? ""
-                              : " ( "}
-                            {data.name_chemical_eng == "null"
-                              ? ""
-                              : data.name_chemical_eng}
-                            {data.name_chemical == "null" &&
-                            data.name_chemical_eng == "null"
-                              ? ""
-                              : " ) "}
-                          </td>
-                          <td>{data.eu_mrl == "null" ? "" : data.eu_mrl}</td>
-                          <td>
-                            {data.time == "null" ? "" : data.time}{" "}
-                            {data.unit == "null" ? "" : data.unit}
-                          </td>
-                          <td>
-                            {data.cc == "null" ? "" : data.cc}
-                            {data.cc == "null" && data.liter == "null"
-                              ? ""
-                              : " CC / "}
-                            {data.liter == "null" ? "" : data.liter}
-                            {" L"}
-                          </td>
-                          <td>{data.note == "null" ? "" : data.note}</td>
-                          <td>
-                            {data.date_start == "null" ? "" : data.date_start}
-                          </td>
-                          <td>
-                            {data.date_end == "null" ? "" : data.date_end}
-                          </td>
-                          <td
-                            style={{
-                              color: "white",
-                              backgroundColor:
-                                data.status_check == "Success"
-                                  ? "green"
-                                  : "red",
-                            }}
-                          >
-                            <center>{data.status_check}</center>
-                          </td>
-                        </tr>
-                      </tbody>
-                    ))}
-                  </table> */}
                 </div>
               </div>
             </div>
