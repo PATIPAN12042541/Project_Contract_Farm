dd2a24b026a2c3e377144a7373cfca83069f0339
PATIPAN12042541 <patiphan@lobo.co.th>
2022-05-30 10:23:28 +0700

    commit

--- src/components/System_overview/System_overview.js
+++ src/components/System_overview/System_overview.js
@@ -17,55 +17,7 @@ const System_overview = () => {
     getOverview();
   }, []);
 
-  //////////////////  Start filter /////////////////////////
-
-  const TextField = styled.input`
-    height: 32px;
-    width: 200px;
-    border-radius: 3px;
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    border: 1px solid #e5e5e5;
-    padding: 0 32px 0 16px;
-
-    &:hover {
-      cursor: pointer;
-    }
-  `;
-
-  const ClearButton = styled(Button)`
-    border-top-left-radius: 0;
-    border-bottom-left-radius: 0;
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
-    height: 34px;
-    width: 32px;
-    text-align: center;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `;
-
-  const FilterComponent = ({ filterText, onFilter, onClear }) => (
-    <>
-      <TextField
-        id="search"
-        type="text"
-        placeholder="Filter By Name"
-        aria-label="Search Input"
-        value={filterText}
-        onChange={onFilter}
-      />
-      <ClearButton type="button" onClick={onClear}>
-        X
-      </ClearButton>
-    </>
-  );
-  //////////////////  End filter /////////////////////////
-
-  //////////////////  Start header colums /////////////////////////
+  // header columns
   const columns = [
     {
       name: "โซนเพาะปลูก",
@@ -135,34 +87,6 @@ const System_overview = () => {
     },
   ];
 
-  //////////////////  End header colums /////////////////////////
-  const Filtering = () => {
-    const [filterText, setFilterText] = React.useState("");
-    const [resetPaginationToggle, setResetPaginationToggle] =
-      React.useState(false);
-    const filteredItems = Overview.filter(
-      (item) =>
-        item.name && item.name.toLowerCase().includes(filterText.toLowerCase())
-    );
-
-    const subHeaderComponentMemo = useMemo(() => {
-      const handleClear = () => {
-        if (filterText) {
-          setResetPaginationToggle(!resetPaginationToggle);
-          setFilterText("");
-        }
-      };
-
-      return (
-        <FilterComponent
-          onFilter={(e) => setFilterText(e.target.value)}
-          onClear={handleClear}
-          filterText={filterText}
-        />
-      );
-    }, [filterText, resetPaginationToggle]);
-  };
-
   return (
     <div className="content-wrapper">
       <section className="content-header">
@@ -181,12 +105,7 @@ const System_overview = () => {
                   <DataTable
                     title="ข้อมูลทั้งหมด"
                     columns={columns}
-                    data={filteredItems}
-                    paginationResetDefaultPage={resetPaginationToggle} // optionally, a hook to reset pagination to page 1
-                    subHeader
-                    subHeaderComponent={subHeaderComponentMemo}
-                    selectableRows
-                    persistTableHead
+                    data={Overview}
                     fixedHeader
                     pagination
                   />
@@ -200,7 +119,4 @@ const System_overview = () => {
   );
 };
 
-export default {
-  System_overview,
-  component: Filtering,
-};
+export default System_overview;
