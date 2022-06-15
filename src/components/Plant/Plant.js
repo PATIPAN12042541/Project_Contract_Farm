72152f533a566cc78092fd9ead7f3812388fc3a4
PATIPAN12042541 <patiphan@lobo.co.th>
2022-06-15 10:02:18 +0700

    commit

--- src/components/Plant/Plant.js
+++ src/components/Plant/Plant.js
@@ -57,67 +57,67 @@ const Plant = (props) => {
                   <div className="row">
                     {datadetail.map((data, index) => (
                       <div className="col-md-12" key={index}>
-                        <Link
+                        {/* <Link
                           to={{
                             pathname: `/Page_chemical/${data.id_plants}`,
                             state: { id: data.id_plants },
                           }}
                           params={data.id_plants}
                           className="text-white"
-                        >
-                          <div className="card mb-12 bg-gradient-white">
-                            <div className="container">
-                              <div className="position-relative">
-                                {data.status_check == "0" ? (
-                                  <div className="ribbon-wrapper ribbon-lg">
-                                    <div className="ribbon bg-success text-lg">
-                                      เสร็จสิ้น
-                                    </div>
+                        > */}
+                        <div className="card mb-12 bg-gradient-white">
+                          <div className="container">
+                            <div className="position-relative">
+                              {data.status_check == "0" ? (
+                                <div className="ribbon-wrapper ribbon-lg">
+                                  <div className="ribbon bg-success text-lg">
+                                    เสร็จสิ้น
                                   </div>
-                                ) : moment(new Date()).format("DD-MM-YYYY") >
-                                  data.end_date_plant ? (
-                                  <div className="ribbon-wrapper ribbon-lg">
-                                    <div
-                                      className="ribbon bg-danger text-lg"
-                                      onLoad={checkdate(data.end_date_plant)}
-                                    >
-                                      เกินเวลาที่กำหนด
-                                    </div>
+                                </div>
+                              ) : moment(new Date()).format("DD-MM-YYYY") >
+                                data.end_date_plant ? (
+                                <div className="ribbon-wrapper ribbon-lg">
+                                  <div
+                                    className="ribbon bg-danger text-lg"
+                                    onClick={checkdate(data.end_date_plant)}
+                                  >
+                                    เกินเวลาที่กำหนด
                                   </div>
-                                ) : (
-                                  ""
+                                </div>
+                              ) : (
+                                ""
+                              )}
+                              <div className="text-block-code">
+                                {data.zone_name + "-" + data.id_name_plant}
+                              </div>
+                              <div className="text-block-Plant">
+                                {data.name_plant}
+                              </div>
+                              <div className="text-block-stdate">
+                                วันที่เริ่มปลูก :{" "}
+                                {moment(data.start_date_plant).format(
+                                  "DD-MM-YYYY"
                                 )}
-                                <div className="text-block-code">
-                                  {data.zone_name + "-" + data.id_name_plant}
-                                </div>
-                                <div className="text-block-Plant">
-                                  {data.name_plant}
-                                </div>
-                                <div className="text-block-stdate">
-                                  วันที่เริ่มปลูก :{" "}
-                                  {moment(data.start_date_plant).format(
-                                    "DD-MM-YYYY"
-                                  )}
-                                </div>
-                                <div className="text-block-eddate">
-                                  วันที่สิ้นสุด :{" "}
-                                  {moment(data.end_date_plant).format(
-                                    "DD-MM-YYYY"
-                                  )}
-                                </div>
+                              </div>
+                              <div className="text-block-eddate">
+                                วันที่สิ้นสุด :{" "}
+                                {moment(data.end_date_plant).format(
+                                  "DD-MM-YYYY"
+                                )}
+                              </div>
 
-                                <img
-                                  className="ima-size card-img-top"
-                                  src={data.plant_image}
-                                />
-                                <div className="text-block-name">
-                                  ผู้รับผิดชอบ : นาย {data.name} นามสกุล{" "}
-                                  {data.last_name}
-                                </div>
+                              <img
+                                className="ima-size card-img-top"
+                                src={data.plant_image}
+                              />
+                              <div className="text-block-name">
+                                ผู้รับผิดชอบ : นาย {data.name} นามสกุล{" "}
+                                {data.last_name}
                               </div>
                             </div>
                           </div>
-                        </Link>
+                        </div>
+                        {/* </Link> */}
                       </div>
                     ))}
                   </div>
