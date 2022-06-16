23ed7fe59ba829a555f74bcca096cc5e4b2437ff
PATIPAN12042541 <patiphan@lobo.co.th>
2022-06-16 15:20:55 +0700

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,8 +3,16 @@ import "./CSS/Home.css";
 
 const Home = () => {
   return (
-    <div className="content-wrapper bg-image">
-      <div className=""></div>
+    <div className="content-wrapper bg-image container">
+      <div className="text-block">
+        <p>
+          <i
+            className="fas fa-seedling fa-2x me-3 pt-5 mt-xl-4"
+            style={{ color: "white" }}
+          />
+          Contract Farming
+        </p>
+      </div>
     </div>
   );
 };
