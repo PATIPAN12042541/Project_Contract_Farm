cbde7a94c13b72c37003f3889f8a7a1ad58d22b2
PATIPAN12042541 <patiphan@lobo.co.th>
2022-06-20 17:09:56 +0700

    commit

--- src/components/Manage_data/Edit_data.js
+++ src/components/Manage_data/Edit_data.js
@@ -613,9 +613,7 @@ const Edit_data = (props) => {
           {" "}
           <div className="form-group">
             <div className="row">
-              <div className="col-sm-2">
-                <label>สถานะ</label>
-              </div>
+              <div className="col-sm-2">สถานะ</div>
               <div className="col-sm-10">
                 <select className="custom-select form-control-border">
                   {StatusPlant.map((status, index) => {
