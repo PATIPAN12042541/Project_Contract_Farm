fe15f1b2a3859f3a5eb712a389bb615b445b96e1
PATIPAN12042541 <patiphan@lobo.co.th>
2022-06-21 10:49:40 +0700

    commit

--- src/components/Data_detail/Plant_detail.js
+++ src/components/Data_detail/Plant_detail.js
@@ -49,6 +49,9 @@ const Plant_detail = (props) => {
                           src={data.plant_image}
                         />
                       </div>
+                      <div>
+                        <button className="btn btn-success">ยืนยัน</button>
+                      </div>
                     </div>
                   </div>
                 </div>
