49a04354629e769936f445b58a8cd774b7d6ca5e
PATIPAN12042541 <patiphan@lobo.co.th>
2022-06-27 09:11:57 +0700

    commit

--- src/components/Plant_Master/Plant_master.js
+++ src/components/Plant_Master/Plant_master.js
@@ -1,7 +1,64 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
 
 const Plant_master = () => {
-  return <div className="content-wrapper">ffff</div>;
+  return (
+    <div className="content-wrapper">
+      {" "}
+      <section className="content-header">
+        <div className="container-fluid">
+          <div className="row mb-2">
+            <div className="col-sm-12"></div>
+          </div>
+        </div>
+      </section>
+      <section className="content">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="card card-success">
+                <div
+                  className="card-header"
+                  style={{ backgroundColor: "#8CC152" }}
+                >
+                  <center>
+                    <h3 className="card-title">จัดการข้อมูลพืช Master</h3>
+                  </center>
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                    <button className="btn btn-success">เพิ่มข้อมูลพืช</button>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <Table
+                      className="table table-bordered table-hover dataTable dtr-inline"
+                      responsive
+                    >
+                      <thead>
+                        <tr>
+                          <th>ลำดับ</th>
+                          <th>ชื่อพืช (ไทย)</th>
+                          <th>ชื่อพืช (Eng)</th>
+                          <th>รูปภาพ</th>
+                          <th>
+                            <center>สถานะ</center>
+                          </th>
+                          <th>
+                            <center>ลบข้อมูล</center>
+                          </th>
+                        </tr>
+                      </thead>
+                    </Table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Plant_master;
