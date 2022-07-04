50eb0a9647e094dddc45efaf5e8a3f8d20ca290d
PATIPAN12042541 <patiphan@lobo.co.th>
2022-07-04 08:59:31 +0700

    commit

--- src/components/Data_detail/Fertilizer_detail.js
+++ src/components/Data_detail/Fertilizer_detail.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
+import Zoom from "react-medium-image-zoom";
 
 const Fertilizer_detail = (props) => {
   const [FertilizerData, setFertilizerData] = useState([]);
@@ -29,14 +30,29 @@ const Fertilizer_detail = (props) => {
       <section className="content">
         <div className="container-fluid">
           {FertilizerData.map((data, index) => (
-            <div className="row mb-2" key={index}>
-              <div className="col-sm-12" />
+            <div className="row" key={index}>
+              <div className="col-md-12" />
               <div className="card card-info">
                 <div
                   className="card-header"
                   style={{ backgroundColor: "#8CC152" }}
                 >
                   <h1 className="card-title">รายละเอียด{data.name_thai}</h1>
+                </div>
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-12 col-sm-4">
+                      <div className="col-12">
+                        <Zoom>
+                          <img
+                            className="img-fluid"
+                            src={data.path_img}
+                            loading="lazy"
+                          />
+                        </Zoom>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
