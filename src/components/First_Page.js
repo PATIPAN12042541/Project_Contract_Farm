fa4b5d4ae961365e4c85e912bafd87400a7e6292
PATIPAN12042541 <patiphan@lobo.co.th>
2022-07-06 15:53:21 +0700

    commit

--- src/components/First_Page.js
+++ src/components/First_Page.js
@@ -32,9 +32,7 @@ const First_Page = () => {
       <div className="content-header">
         <div className="container-fluid">
           <div className="row mb-2">
-            <div className="col-sm-6">
-              {/* <h1 className="m-0">สภาพอากาศ</h1> */}
-            </div>
+            <div className="col-sm-6"></div>
           </div>
         </div>
       </div>
