4279f5ac26f764b64428a477e204d90175b9e9ff
PATIPAN12042541 <patiphan@lobo.co.th>
2022-07-06 13:38:15 +0700

    commit

--- src/components/FertilizerUnit/FertilizerUnit.js
+++ src/components/FertilizerUnit/FertilizerUnit.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const FertilizerUnit = () => {
-  return <div className="content-wrapper">ทดสอบ</div>;
+  return (
+    <div className="content-wrapper">
+      <section className="content-header">
+        <div className="container-fluid">
+          <div className="row mb-2">
+            <div className="col-sm-12"></div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default FertilizerUnit;
