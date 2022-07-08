700fd3f4c65d5648b4ec9c4d7fd339af0719c6db
PATIPAN12042541 <patiphan@lobo.co.th>
2022-07-08 11:25:29 +0700

    commit

--- src/components/Time_Chemical/TimeChemical.js
+++ src/components/Time_Chemical/TimeChemical.js
@@ -8,27 +8,26 @@ import { BiEditAlt } from "react-icons/bi";
 const TimeChemical = () => {
   const [TimeChemical, setTimeChemical] = useState([]);
   /* ---------------------------------------------------------*/
-  const [open, setopen] = useState(true);
   const [time, setTime] = useState([]);
 
   const ChangeOpen = async (e, id) => {
-
+    console.log(time);
     if (e == 1) {
-        await axios.patch(
-            `${process.env.REACT_APP_API_URL}/getChemical/TimeChemical/updateStatus/${id}`,
-            {
-              status: e,
-              time : time,
-            }
-          );
-          Swal.fire("Completed!", "บันทึกสำเร็จ", "success");
+      await axios.patch(
+        `${process.env.REACT_APP_API_URL}/getChemical/TimeChemical/updateStatus/${id}`,
+        {
+          status: e,
+          time: time,
+        }
+      );
+      Swal.fire("Completed!", "บันทึกสำเร็จ", "success");
     } else {
-        await axios.patch(
-            `${process.env.REACT_APP_API_URL}/getChemical/TimeChemical/updateStatus/${id}`,
-            {
-              status: e,
-            }
-          );
+      await axios.patch(
+        `${process.env.REACT_APP_API_URL}/getChemical/TimeChemical/updateStatus/${id}`,
+        {
+          status: e,
+        }
+      );
     }
     getTimeChemical();
   };
