2213ca969f53aa44d609a07cb9efdebf0082db7e
PATIPAN12042541 <patiphan@lobo.co.th>
2022-08-09 13:59:10 +0700

    commit

--- src/components/Content.js
+++ src/components/Content.js
@@ -33,6 +33,7 @@ const Content = () => {
       const response = await axios.get(
         `${process.env.REACT_APP_API_URL}/user/token`
       );
+      console.log(response);
       setToken(response.data.accessToken);
       const decoded = jwt_decode(response.data.accessToken);
 
