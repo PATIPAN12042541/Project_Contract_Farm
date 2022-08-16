c0b958164038dae41f7c1a1693b327f4727b9394
kobkij <Kobkij@globofoods.com>
2022-08-16 11:08:51 +0700

    commit

--- src/components/Chemical/List_Chemical.js
+++ src/components/Chemical/List_Chemical.js
@@ -12,6 +12,7 @@ import Form from "react-bootstrap/Form";
 import "../CSS/List_chemical.css";
 import { BsTrashFill } from "react-icons/bs";
 import { AiFillEdit } from "react-icons/ai";
+import ReactPaginate from 'react-paginate';
 
 const List_Chemical = () => {
   const [listChemicals, setListChemicals] = useState([]);
@@ -27,6 +28,12 @@ const List_Chemical = () => {
   const [image, setImage] = useState({ preview: "", data: "" });
   const [image_name, setImageName] = useState();
   const navigate = useNavigate();
+
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  // Here we use item offsets; we could also use page offsets
+  // following the API or data you're working with.
+  const [itemOffset, setItemOffset] = useState(0);
 
   const getListTypeChemicals = async () => {
     const response = await axios.get(
@@ -129,7 +136,22 @@ const List_Chemical = () => {
   useEffect(() => {
     getListChemical();
     getListTypeChemicals();
-  }, []);
+
+    // Fetch items from another resources.
+    const endOffset = itemOffset + itemsPerPage;
+    console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+    setCurrentItems(items.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(items.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage]);
+
+  // Invoke when user click to request another page.
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % items.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
 
   const getListChemical = async () => {
     const response = await axios.get(
@@ -459,6 +481,16 @@ const List_Chemical = () => {
                 </div>
               </div>
             </div>
+
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel="next >"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={pageCount}
+              previousLabel="< previous"
+              renderOnZeroPageCount={null}
+            />
           </Form>
         </Modal.Body>
 
