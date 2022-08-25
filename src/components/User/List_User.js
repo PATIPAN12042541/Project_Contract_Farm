7913657c4fd7bada0b7ee2fed4e474f47447530f
kobkij <Kobkij@globofoods.com>
2022-08-25 11:57:52 +0700

    commit

--- src/components/User/List_User.js
+++ src/components/User/List_User.js
@@ -33,7 +33,6 @@ export const List_User = () => {
             `${process.env.REACT_APP_API_URL}/User/getUsersByDev`
         );
         setListUsers(response.data);
-        console.log(role_id);
     };
 
   // Search Item
