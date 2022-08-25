14ce7820ca2d636b72a75a9c5b71b0bf08f1da0b
kobkij <Kobkij@globofoods.com>
2022-08-25 10:12:21 +0700

    commit

--- backend/controllers/GroupUserView.js
+++ backend/controllers/GroupUserView.js
@@ -3,9 +3,6 @@ import GroupUsersView from "../models/GroupUsersView.js"
 export const getViewUsersByDev = async(req, res) => {
     try {
         const GroupUsers = await GroupUsersView.findAll({
-            where:{
-                role_id : 3,
-            },
             attributes:['ID','NAME','LAST_NAME','GROUP_ID','GROUP_NAME']
         });
         res.json(GroupUsers);
