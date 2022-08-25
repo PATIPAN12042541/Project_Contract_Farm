3e926596505eae60e597612fd7baaca92cced1a8
kobkij <Kobkij@globofoods.com>
2022-08-25 08:47:12 +0700

    commit

---
+++ src/components/User/List_User.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+export const List_User = () => {
+  return (
+      <div className="content-wrapper">
+          <section className="content">
+              <div className="container-fluid">
+                  <div className="row">
+                      <div className="col-md-12">
+                          <div className="card card-success">
+                              <div
+                                  className="card-header"
+                                  style={{ backgroundColor: "#8CC152" }}
+                              >
+                                  <center>
+                                      <h3 className="card-title">ข้อมูลผู้ใช้งานระบบ</h3>
+                                  </center>
+                              </div>
+                              <div className="card-body">
+                                  <div className="row">
+                                      <div className="col-md-6">
+                                          <Button variant="success">
+                                              เพิ่มข้อมูลผู้ใช้งานระบบ
+                                          </Button>
+                                      </div>
+                                      <div className="col-md-6">
+                                          <input
+                                              type="text"
+                                              className="form-control"
+                                              placeholder="ค้นหา"
+                                          />
+                                      </div>
+                                  </div>
+                                  <hr />
+                                  <div className="row">
+                                      <Table
+                                          className="table table-bordered table-hover dataTable dtr-inline"
+                                          responsive
+                                      >
+                                          <thead>
+                                              <tr>
+                                                  <th>ลำดับ</th>
+                                                  <th>ประเภทสารเคมี</th>
+                                                  <th>ชื่อสารเคมี (ไทย)</th>
+                                                  <th>ชื่อสารเคมี (Eng)</th>
+                                                  <th>ER MUL</th>
+                                                  <th>รูปภาพ</th>
+                                                  <th>Active</th>
+                                                  <th>แก้ไขข้อมูล</th>
+                                                  <th>ลบข้อมูล</th>
+                                              </tr>
+                                          </thead>
+                                          <tbody>
+                                              
+                                          </tbody>
+                                      </Table>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
+      </div>
+  )
+}
