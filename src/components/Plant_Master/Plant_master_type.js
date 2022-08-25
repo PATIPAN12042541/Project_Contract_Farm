5667624df3a8c84218a0aaacb22f17111289cc7a
PATIPAN12042541 <patiphan@lobo.co.th>
2022-08-25 15:00:33 +0700

    commit

--- src/components/Plant_Master/Plant_master_type.js
+++ src/components/Plant_Master/Plant_master_type.js
@@ -6,8 +6,23 @@ import Button from "react-bootstrap/Button";
 import Table from "react-bootstrap/Table";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
+import { BsTrashFill } from "react-icons/bs";
 
 const Plant_master_type = () => {
+  const [TypeMasterPlant, setTypeMasterPlant] = useState([]);
+
+  const getTypeMasterPlant = async () => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API_URL}/getplant/plant/getPlantMasterType`
+    );
+    setTypeMasterPlant(response.data);
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    getTypeMasterPlant();
+  }, []);
+
   return (
     <div className="content-wrapper">
       <section className="content-header">
@@ -35,6 +50,37 @@ const Plant_master_type = () => {
                     <Button variant="success">เพิ่มประเภทพืช</Button>
                   </div>
                   <hr />
+                  <div className="row">
+                    <Table
+                      className="table table-bordered table-hover dataTable dtr-inline"
+                      responsive
+                    >
+                      <thead>
+                        <tr>
+                          <th>ลำดับ</th>
+                          <th>ประเภทพืช Master</th>
+                          <th>
+                            <center>แก้ไขข้อมูล</center>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {TypeMasterPlant.map((data, index) => (
+                          <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{data.type_plant_name}</td>
+                            <td>
+                              <center>
+                                <Button variant="danger">
+                                  <BsTrashFill />
+                                </Button>
+                              </center>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </Table>
+                  </div>
                 </div>
               </div>
             </div>
