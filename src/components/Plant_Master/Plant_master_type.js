b9fb43fd1c873f67db698d8f30aa0c1d0cac646d
PATIPAN12042541 <patiphan@lobo.co.th>
2022-08-25 16:14:06 +0700

    commit

--- src/components/Plant_Master/Plant_master_type.js
+++ src/components/Plant_Master/Plant_master_type.js
@@ -206,17 +206,15 @@ const Plant_master_type = () => {
 
       {/*  Edit Plant Master */}
       <Modal show={show2} onHide={handleClose2}>
-        <Modal.Header>
-          <Modal.Title
-            style={{
-              backgroundColor: "rgb(140, 193, 82)",
-              color: "#FFFFFF",
-              fontSize: "24px",
-              borderLine: "none",
-            }}
-          >
-            แก้ไขประเภทพืช Master
-          </Modal.Title>
+        <Modal.Header
+          style={{
+            backgroundColor: "rgb(140, 193, 82)",
+            color: "#FFFFFF",
+            fontSize: "24px",
+            borderLine: "none",
+          }}
+        >
+          <Modal.Title>แก้ไขประเภทพืช Master</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form className="form-horizontal">
