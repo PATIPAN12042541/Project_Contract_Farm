7473b31ac3a0b8522a3465ce66474bbf695ab4de
kobkij <Kobkij@globofoods.com>
2022-08-26 14:52:19 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -34,7 +34,8 @@ import FertilizerUnit from "./components/FertilizerUnit/FertilizerUnit.js";
 import TimeChemical from "./components/Time_Chemical/TimeChemical.js";
 import { List_User } from "./components/User/List_User.js";
 import Plant_master_type from "./components/Plant_Master/Plant_master_type";
-import React from "react";
+import React,{useState} from "react";
+import axios from 'axios';
 
 const [token, setToken] = useState("");
 
