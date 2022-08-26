db6d30a6bac7a0a2e5b20e607bc029eac0db8259
kobkij <Kobkij@globofoods.com>
2022-08-26 08:47:29 +0700

    commit

--- src/components/User/List_User.js
+++ src/components/User/List_User.js
@@ -9,7 +9,6 @@ import Pagination from "../Pagination/Pagination.js";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import '../Pagination/style.scss';
-import { AuthContext } from '../../App.js';
 
 let PageSize = 5;
 
@@ -31,8 +30,6 @@ export const List_User = () => {
   const [lastName, setLastName] = useState("");
   const [roleID, setRoleID] = useState("");
   const Nav = useNavigate();
-
-  const auth = useContext(AuthContext);
 
     const getListUser = async () => {
         /*if (roleid == 1) {
