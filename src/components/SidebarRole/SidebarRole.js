e8f99fc08744e4c03f7bb9f00b5534639f914330
kobkij <Kobkij@globofoods.com>
2022-08-26 13:42:40 +0700

    commit

--- src/components/SidebarRole/SidebarRole.js
+++ src/components/SidebarRole/SidebarRole.js
@@ -8,6 +8,7 @@ import logo from "./logo/sprout.png";
 import user6 from "./logo/user6-128x128.jpg";
 import { ListGroup } from 'react-bootstrap';
 import Image from "react-bootstrap/Image";
+import { AuthContext } from '../../App';
 
 const SidebarRole = () => {
   const [name, setName] = useState("");
@@ -18,6 +19,8 @@ const SidebarRole = () => {
   const [menurole, setMenuRole] = useState([]);
   const [submenurole, setSubMenusRole] = useState([]);
   const history = useNavigate();
+
+  const {authState,authDispatch} = useContext(AuthContext);
   
   useEffect(() => {
     refreshToken();
@@ -40,6 +43,8 @@ const SidebarRole = () => {
 
       roleMenu(decoded.role_id);
       subMenu1(decoded.role_id);
+
+      authDispatch({type:'login',payload:decoded.last_name})
     } catch (error) {
       if (error.response) {
         history("/");
