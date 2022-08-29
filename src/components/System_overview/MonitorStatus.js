6fe205d9bcd6015a0e6e65aa9b678f9274281dce
PATIPAN12042541 <patiphan@lobo.co.th>
2022-08-29 09:33:27 +0700

    commit

--- src/components/System_overview/MonitorStatus.js
+++ src/components/System_overview/MonitorStatus.js
@@ -29,8 +29,10 @@ const MonitorStatus = () => {
                           <i className="far fa-thumbs-up"></i>
                         </span>
                         <div className="info-box-content">
-                          <span className="info-box-text">Likes</span>
-                          <span className="info-box-number">41,410</span>
+                          <span className="info-box-text">
+                            สถานะที่เสร็จสิ้นเเล้ว
+                          </span>
+                          <span className="info-box-number">200 แปลง</span>
                           <div className="progress">
                             <div
                               className="progress-bar"
@@ -38,7 +40,7 @@ const MonitorStatus = () => {
                             ></div>
                           </div>
                           <span className="progress-description">
-                            70% Increase in 30 Days
+                            จากทั้งหมด 300 แปลง
                           </span>
                         </div>
                       </div>
@@ -49,8 +51,10 @@ const MonitorStatus = () => {
                           <i className="far fa-calendar-alt"></i>
                         </span>
                         <div className="info-box-content">
-                          <span className="info-box-text">Events</span>
-                          <span className="info-box-number">41,410</span>
+                          <span className="info-box-text">
+                            สถานะกำลังดำเนินการ
+                          </span>
+                          <span className="info-box-number">99 แปลง</span>
                           <div className="progress">
                             <div
                               className="progress-bar"
@@ -58,7 +62,7 @@ const MonitorStatus = () => {
                             ></div>
                           </div>
                           <span className="progress-description">
-                            70% Increase in 30 Days
+                            จากงานทั้งหมด 300 งาน
                           </span>
                         </div>
                       </div>
@@ -69,8 +73,10 @@ const MonitorStatus = () => {
                           <i className="fas fa-comments"></i>
                         </span>
                         <div className="info-box-content">
-                          <span className="info-box-text">Comments</span>
-                          <span className="info-box-number">41,410</span>
+                          <span className="info-box-text">
+                            สถานะเกินเวลาที่กำหนด
+                          </span>
+                          <span className="info-box-number">1 แปลง</span>
                           <div className="progress">
                             <div
                               className="progress-bar"
@@ -78,38 +84,50 @@ const MonitorStatus = () => {
                             ></div>
                           </div>
                           <span className="progress-description">
-                            70% Increase in 30 Days
+                            จากงานทั้งหมด 300 งาน
                           </span>
                         </div>
                       </div>
                     </div>
                   </div>
                   <div className="row">
-                    <div className="col-4">
+                    <div className="col-3">
                       รหัสโซนเพาะปลูก
                       <select className="custom-select form-control-border">
+                        <option>----เลือกข้อมูล----</option>
                         <option>A1</option>
                         <option>A2</option>
                         <option>A3</option>
                         <option>A4</option>
                       </select>
                     </div>
-                    <div className="col-4">
+                    <div className="col-3">
                       ชนิดเเปลงเพาะปลูก
                       <select className="custom-select form-control-border">
+                        <option>----เลือกข้อมูล----</option>
                         <option>แปลงมะเขือ</option>
                         <option>แปลงแตงกวา</option>
                         <option>แปลงคะน้า</option>
                         <option>แปลงมะม่วง</option>
                       </select>
                     </div>
-                    <div className="col-4">
+                    <div className="col-3">
                       ชื่อผู้รับผิดชอบ
                       <select className="custom-select form-control-border">
+                        <option>----เลือกข้อมูล----</option>
                         <option>นายปฏิภาณ ศรีทองคำ</option>
                         <option>นายปฏิภาณ ศรีทองคำ</option>
                         <option>นายปฏิภาณ ศรีทองคำ</option>
                         <option>นายปฏิภาณ ศรีทองคำ</option>
+                      </select>
+                    </div>
+                    <div className="col-3">
+                      สถานะ
+                      <select className="custom-select form-control-border">
+                        <option>----เลือกข้อมูล----</option>
+                        <option>หมดเวลา</option>
+                        <option>กำลังดำเนินการ</option>
+                        <option>เสร็จสิ้น</option>
                       </select>
                     </div>
                   </div>
@@ -124,7 +142,8 @@ const MonitorStatus = () => {
                           <th>แปลงเพาะปลูก</th>
                           <th>ผู้ดูเเล</th>
                           <th>สถานะ</th>
-                          <th style={{ width: "40px" }}>ความสำเร็จ</th>
+                          <th>ความคืบหน้า</th>
+                          <th>เปอร์เซ็น</th>
                         </tr>
                       </thead>
                       <tbody>
@@ -134,16 +153,17 @@ const MonitorStatus = () => {
                           <td>1</td>
                           <td>แปลงมะเขือ</td>
                           <td>นายปฏิภาณ ศรีทองคำ</td>
+                          <td>หมดเวลา</td>
                           <td>
                             <div className="progress progress-xs">
                               <div
                                 className="progress-bar bg-danger"
-                                style={{ width: "55%" }}
+                                style={{ width: "0%" }}
                               ></div>
                             </div>
                           </td>
                           <td>
-                            <span className="badge bg-danger">55%</span>
+                            <span className="badge bg-danger">0%</span>
                           </td>
                         </tr>
                         <tr>
@@ -152,52 +172,36 @@ const MonitorStatus = () => {
                           <td>2</td>
                           <td>แปลงแตงกวา</td>
                           <td>นายปฏิภาณ ศรีทองคำ</td>
+                          <td>กำลังดำเนินการ</td>
                           <td>
                             <div className="progress progress-xs">
                               <div
                                 className="progress-bar bg-warning"
-                                style={{ width: "70%" }}
+                                style={{ width: "50%" }}
                               ></div>
                             </div>
                           </td>
                           <td>
-                            <span className="badge bg-warning">70%</span>
+                            <span className="badge bg-warning">50%</span>
                           </td>
                         </tr>
                         <tr>
                           <td>3.</td>
-                          <td>A3</td>
-                          <td>3</td>
-                          <td>แปลงคะน้า</td>
-                          <td>นายปฏิภาณ ศรีทองคำ</td>
-                          <td>
-                            <div className="progress progress-xs progress-striped active">
-                              <div
-                                className="progress-bar bg-primary"
-                                style={{ width: "30%" }}
-                              ></div>
-                            </div>
-                          </td>
-                          <td>
-                            <span className="badge bg-primary">30%</span>
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>4.</td>
                           <td>A4</td>
                           <td>1</td>
                           <td>แปลงมะม่วง</td>
                           <td>นายปฏิภาณ ศรีทองคำ</td>
+                          <td>เสร็จสิ้น</td>
                           <td>
                             <div className="progress progress-xs progress-striped active">
                               <div
                                 className="progress-bar bg-success"
-                                style={{ width: "90%" }}
+                                style={{ width: "100%" }}
                               ></div>
                             </div>
                           </td>
                           <td>
-                            <span className="badge bg-success">90%</span>
+                            <span className="badge bg-success">100%</span>
                           </td>
                         </tr>
                       </tbody>
