b6845aa4fe92b392900cf20ee2918802713abf85
kobkij <Kobkij@globofoods.com>
2022-08-29 13:52:15 +0700

    commit

--- src/components/User/List_User.js
+++ src/components/User/List_User.js
@@ -198,7 +198,10 @@ export const List_User = () => {
                                                       <td>
                                                           <center>
                                                               <Link to={``}>
-                                                                  <Button onClick={handleShowUpdate}
+                                                                  <Button onClick={()=>{
+                                                                    handleShowUpdate()
+                                                                    getUserById(listUsers.id)
+                                                                  }}
                                                                       variant="warning"
                                                                       style={{ color: "#ffff" }}
                                                                   >
@@ -364,7 +367,7 @@ export const List_User = () => {
               </Modal.Footer>
           </Modal>
 
-          <Modal show={showUpdate} onHide={handleCloseUpdate} onShow={getUserById(listUsers.id)}>
+          <Modal show={showUpdate} onHide={handleCloseUpdate} >
               <Modal.Header
                   style={{
                       backgroundColor: "rgb(140, 193, 82)",
@@ -383,7 +386,7 @@ export const List_User = () => {
                                   type="text"
                                   className="form-control"
                                   placeholder="Username"
-                                  value={username}
+                                  Value={username}
                                   onChange={(e) => setUserName(e.target.value)}
                               />
                               <div className="input-group-append">
@@ -391,41 +394,13 @@ export const List_User = () => {
                                       <span className="fas fa-user" />
                                   </div>
                               </div>
-                          </div>
-                          <div className="input-group mb-3">
-                              <input
-                                  type="password"
-                                  className="form-control"
-                                  placeholder="Password"
-                                  value={password}
-                                  onChange={(e) => setPassword(e.target.value)}
-                              />
-                              <div className="input-group-append">
-                                  <div className="input-group-text">
-                                      <span className="fas fa-lock" />
-                                  </div>
-                              </div>
-                          </div>
-                          <div className="input-group mb-3 ">
-                              <input
-                                  type="password"
-                                  className="form-control"
-                                  placeholder="Confirm password"
-                                  value={confirmPassword}
-                                  onChange={(e) => setConfirmPassword(e.target.value)}
-                              />
-                              <div className="input-group-append">
-                                  <div className="input-group-text">
-                                      <span className="fas fa-lock" />
-                                  </div>
-                              </div>
-                          </div>
+                          </div>                        
                           <div className="input-group mb-3">
                               <input
                                   type="text"
                                   className="form-control"
                                   placeholder="Name"
-                                  value={name}
+                                  Value={name}
                                   onChange={(e) => setName(e.target.value)}
                               />
                           </div>
@@ -434,7 +409,7 @@ export const List_User = () => {
                                   type="text"
                                   className="form-control"
                                   placeholder="Last Name"
-                                  value={lastName}
+                                  Value={lastName}
                                   onChange={(e) => setLastName(e.target.value)}
                               />
                           </div>
@@ -447,7 +422,7 @@ export const List_User = () => {
                               >
                                   <option>--เลือก Role--</option>
                                   {rolegroup.map((item) => (
-                                      <option key={item.id} value={item.id}>
+                                      <option key={item.id} value={roleID}>
                                           {item.role_group_name}
                                       </option>
                                   ))}
@@ -459,6 +434,7 @@ export const List_User = () => {
                                   <input
                                       type="checkbox"
                                       id="custom-switch"
+                                      checked={checked}
                                       onChange={(e) => {
                                           setChecked(!checked);
                                       }}
