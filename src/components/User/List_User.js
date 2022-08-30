277eb0b6f16d422bcffb29c7665f5b2ac2fbb6c9
kobkij <Kobkij@globofoods.com>
2022-08-30 15:44:05 +0700

    commit

--- src/components/User/List_User.js
+++ src/components/User/List_User.js
@@ -504,16 +504,6 @@ export const List_User = () => {
                                   />
                               </div>
                           </div>
-
-                          <div class="bootstrap-switch bootstrap-switch-wrapper bootstrap-switch-focused bootstrap-switch-animate bootstrap-switch-on" style="width: 86px;">
-                            <div class="bootstrap-switch-container" style={"width: 126px; margin-left: 0px;"}>
-                                <span class="bootstrap-switch-handle-on bootstrap-switch-primary" style={"width: 42px;"}>ON</span>
-                                <span class="bootstrap-switch-label" style={"width: 42px;"}>&nbsp;</span>
-                                <span class="bootstrap-switch-handle-off bootstrap-switch-default" style={"width: 42px;"}>OFF</span>
-                                <input type="checkbox" name="my-checkbox" checked="" data-bootstrap-switch="" />
-                            </div>
-                        </div>
-
                       </div>
                   </Form>
               </Modal.Body>
