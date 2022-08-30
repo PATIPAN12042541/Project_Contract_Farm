28df97b574ca0827bbb00ec692664ff93a56560a
kobkij <Kobkij@globofoods.com>
2022-08-30 11:43:18 +0700

    commit

--- backend/models/RoleGroup.js
+++ backend/models/RoleGroup.js
@@ -1,6 +1,5 @@
 import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
-import Users from "./UserModel.js";
 const { DataTypes } = Sequelize;
  const RoleGroup = db.define('role_group',{
     role_group_name:{
