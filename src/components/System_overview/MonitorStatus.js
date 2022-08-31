939d9b303ad8c7e1376592352d95197af34be6d2
PATIPAN12042541 <patiphan@lobo.co.th>
2022-08-31 13:25:16 +0700

    commit

--- src/components/System_overview/MonitorStatus.js
+++ src/components/System_overview/MonitorStatus.js
@@ -19,6 +19,8 @@ const MonitorStatus = () => {
 
   const searchItems = (searchValue) => {
     setDataFilter(getstatusDetail);
+
+    console.log(getDataFilter);
     if (searchValue !== "") {
       if (searchValue == 1) {
         const filteredData = getDataFilter.filter((data) => data.Danger_ === 1);
