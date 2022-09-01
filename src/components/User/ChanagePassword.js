c9c01c3c53d8e085064f1f5b8fe33f66f5bfd440
kobkij <Kobkij@globofoods.com>
2022-09-01 15:29:35 +0700

    commit

--- src/components/User/ChanagePassword.js
+++ src/components/User/ChanagePassword.js
@@ -1,18 +1,12 @@
-import React,{ useState, useEffect ,useMemo} from 'react'
+import React,{ useState, useEffect} from 'react'
 import Button from 'react-bootstrap/Button'
 import Table from 'react-bootstrap/Table'
 import axios from "axios";
-import { Link, useNavigate,useParams } from "react-router-dom";
-import { AiFillEdit } from "react-icons/ai";
-import Pagination from "../Pagination/Pagination.js";
+import {useNavigate,useParams } from "react-router-dom";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import Swal from "sweetalert2";
-import Image from "react-bootstrap/Image";
 import jwt_decode from "jwt-decode";
-import Switch from "react-switch";
-
-let PageSize = 5;
 
 const ChanagePassword = () => {
   const [listUsers, setListUsers] = useState([]);
