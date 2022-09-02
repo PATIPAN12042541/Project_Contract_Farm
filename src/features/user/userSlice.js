2be4e9d99f0ec9c7edd288c69cc2e14ebefed3a2
kobkij <Kobkij@globofoods.com>
2022-09-02 15:51:45 +0700

    commit

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -10,7 +10,7 @@ export const userSlice = createSlice({
         // doesn't actually mutate the state because it uses the Immer library,
         // which detects changes to a "draft state" and produces a brand new
         // immutable state based off those changes
-        state.roleID = action.payload.roleID
+        state.roleID = '1'
       },
       logout: (state,action) => {
         state.username = ''
