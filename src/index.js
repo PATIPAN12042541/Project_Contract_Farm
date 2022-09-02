8fb5dfcc8b527ebd09b32e3a9f864d4f9223f370
kobkij <Kobkij@globofoods.com>
2022-09-02 17:13:33 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from "axios";
-import { Provider } from 'react-redux'
 
 axios.defaults.withCredentials = true;
 
