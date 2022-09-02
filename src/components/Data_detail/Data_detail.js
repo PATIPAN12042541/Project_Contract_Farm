b1bbe6c350d31323b93887dcde8e175ab97914ec
PATIPAN12042541 <patiphan@lobo.co.th>
2022-09-02 09:19:24 +0700

    commit

--- src/components/Data_detail/Data_detail.js
+++ src/components/Data_detail/Data_detail.js
@@ -203,7 +203,29 @@ const Data_detail = (props) => {
           ))}
         </div>
         <div className="row">
-          <div>ทดสอบระบบ</div>
+          <section className="content-header">
+            <div className="container-fluid">
+              <div className="row mb-2">
+                <div className="col-sm-12" />
+              </div>
+            </div>
+          </section>
+          <section className="content">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="card">
+                    <div
+                      className="card-header"
+                      style={{ backgroundColor: "#8CC152", color: "#FFFFFF" }}
+                    >
+                      <h3 className="card-title">ตรวจสอบสถานะการทำงาน</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </section>
     </div>
