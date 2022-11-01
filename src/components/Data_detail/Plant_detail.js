f4689ccc939c9e9cece0600df702ec5e68cd9b24
PATIPAN12042541 <patiphan@lobo.co.th>
2022-11-01 08:27:10 +0700

    commit

--- src/components/Data_detail/Plant_detail.js
+++ src/components/Data_detail/Plant_detail.js
@@ -201,19 +201,17 @@ const Plant_detail = (props) => {
                           </div>
                         </div>
                         <div className="col-md-12">
-                          <section className="container">
-                            <div {...getRootProps({ className: "dropzone" })}>
-                              <input {...getInputProps()} />
-                              <p>
-                                Drag 'n' drop some files here, or click to
-                                select files
-                              </p>
-                            </div>
-                            <aside>
-                              <h4>Files</h4>
-                              <ul>{files}</ul>
-                            </aside>
-                          </section>
+                          <div {...getRootProps({ className: "dropzone" })}>
+                            <input {...getInputProps()} />
+                            <p>
+                              Drag 'n' drop some files here, or click to select
+                              files
+                            </p>
+                          </div>
+                          <aside>
+                            <h4>Files</h4>
+                            <ul>{files}</ul>
+                          </aside>
                         </div>
                       </div>
                     </div>
