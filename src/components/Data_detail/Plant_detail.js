42e1cefc53686e5267d308e0196131826e907dfa
PATIPAN12042541 <patiphan@lobo.co.th>
2022-11-01 08:44:56 +0700

    commit

--- src/components/Data_detail/Plant_detail.js
+++ src/components/Data_detail/Plant_detail.js
@@ -275,12 +275,13 @@ const Plant_detail = (props) => {
           <p>Drag 'n' drop some files here, or click to select files</p>
           <em>(Only *.jpeg and *.png images will be accepted)</em>
         </div>
-        <aside>
+
+        {/* <aside>
           <h4>Accepted files</h4>
           <ul>{acceptedFileItems}</ul>
           <h4>Rejected files</h4>
           <ul>{fileRejectionItems}</ul>
-        </aside>
+        </aside> */}
       </section>
     </div>
   );
