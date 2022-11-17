21084e2e3601cc01f3d21b048a32500babf866dc
PATIPAN12042541 <patiphan@lobo.co.th>
2022-11-17 16:04:02 +0700

    commit

--- src/components/Report_Defect/Report_Defect.js
+++ src/components/Report_Defect/Report_Defect.js
@@ -19,53 +19,6 @@ const Report_Defect = () => {
 
   return (
     <div className="content-wrapper">
-      {/* <section className="content-header">
-        <div className="container-fluid">
-          <div className="row mb-2">
-            <div className="col-sm-6">
-              <h1>รายงานปัญหา</h1>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section className="content">
-        <div className="card image_bg">
-          <div className="card-body row">
-            <div className="col-5 text-center d-flex align-items-center justify-content-center">
-              <div className="">
-                <h1>
-                  Contract<strong>Farming</strong>
-                </h1>
-                <p className="lead mb-5">report an issue</p>
-              </div>
-            </div>
-            <div className="col-7">
-              <div className="form-group">
-                <label className="inputName">ชื่อแปลง</label>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="form-group">
-                <label>หัวข้อปัญหา</label>
-                <input type="text" className="form-control" />
-              </div>
-              <div className="form-group">
-                <label>ปัญหาที่พบ</label>
-                <textarea className="form-control" rows="4"></textarea>
-              </div>
-              <div className="form-group">
-                <input
-                  type="submit"
-                  className="btn btn-warning"
-                  style={{ color: "white" }}
-                  value={"ส่งรายงานปัญหา"}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
-
       <section className="content-header">
         <div className="container-fluid">
           <div className="row mb-2">
