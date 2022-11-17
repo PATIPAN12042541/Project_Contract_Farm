b7402ed0632ac9aa5604fe308c0c02864f0d4a73
PATIPAN12042541 <patiphan@lobo.co.th>
2022-11-17 10:50:53 +0700

    commit

--- src/components/Report_Defect/Report_Defect.js
+++ src/components/Report_Defect/Report_Defect.js
@@ -60,7 +60,44 @@ const Report_Defect = () => {
           </div>
         </div>
       </section>
-      <section className="content"></section>
+      <section className="content">
+        <div className="card">
+          <div className="card-header">
+            <h3 className="card-title">Bordered Table</h3>
+          </div>
+
+          <div className="card-body">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th style={{ width: "10px" }}>#</th>
+                  <th>Task</th>
+                  <th>Progress</th>
+                  <th style={{ width: "40px" }}>Label</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1.</td>
+                  <td>Update software</td>
+                  <td>
+                    <div className="progress progress-xs">
+                      <div
+                        className="progress-bar progress-bar-danger"
+                        style="width: 55%"
+                      ></div>
+                    </div>
+                  </td>
+                  <td>
+                    <span className="badge bg-danger">55%</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+        </div>
+      </section>
     </div>
   );
 };
