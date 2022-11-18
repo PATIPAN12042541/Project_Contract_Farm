6f741d8b801f5dca3ffde2d8956d4f0c2540b415
kobkij <Kobkij@globofoods.com>
2022-11-18 17:00:00 +0700

    commit

--- src/components/Setting_Menu/SettingMenu.js
+++ src/components/Setting_Menu/SettingMenu.js
@@ -7,7 +7,8 @@ import '../Pagination/style.scss';
 const SettingMenu = () => {
   
     const [rolegroup, setRoleGroup] = useState([]);
-    const [roleID, setRoleID] = useState();
+    const [roleMenuMainID, setRoleMenuMainID] = useState();
+    const [roleMenuParentID, setRoleMenuParentID] = useState();
 
     //Drop Down Role
     const getRole = async () => {
@@ -42,7 +43,7 @@ const SettingMenu = () => {
                                   style={{ backgroundColor: "#8CC152" }}
                               >
                                   <center>
-                                      <h3 className="card-title">ตั้งค่าเมนู</h3>
+                                      <h3 className="card-title">ตั้งค่าเมนูหลัก</h3>
                                   </center>
                               </div>
                               <div className="card-body">
@@ -50,7 +51,7 @@ const SettingMenu = () => {
                                       <select
                                           className="form-control"
                                           onChange={(e) => {
-                                              setRoleID(e.target.value);
+                                            setRoleMenuMainID(e.target.value);
                                           }}
                                       >
                                           <option>--เลือก Role--</option>
@@ -64,7 +65,66 @@ const SettingMenu = () => {
                                   <br />
                                   <div>                              
                                       <Button variant="success">
-                                          เพิ่มเมนูย่อย
+                                          เพิ่มเมนูหลัก
+                                      </Button>
+                                  </div>
+                                  <hr />
+                                  <div className="row">
+                                      <Table
+                                          className="table table-bordered table-hover dataTable dtr-inline"
+                                          responsive
+                                      >
+                                          <thead>
+                                              <tr>
+                                                  <th>ลำดับ</th>
+                                                  <th>ชื่อเมนู</th>
+                                                  <th>ลำดับของเมนู</th>
+                                                  <th>Link</th>
+                                                  <th>
+                                                      <center>แก้ไขข้อมูล</center>
+                                                  </th>
+                                                  <th>Active</th>
+                                              </tr>
+                                          </thead>
+                                          <tbody>
+                                          </tbody>
+                                      </Table>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div className="row">
+                      <div className="col-md-12">
+                          <div className="card card-success">
+                              <div
+                                  className="card-header"
+                                  style={{ backgroundColor: "#8CC152" }}
+                              >
+                                  <center>
+                                      <h3 className="card-title">ตั้งค่าเมนูย่อย</h3>
+                                  </center>
+                              </div>
+                              <div className="card-body">
+                                  <div className="row">
+                                      <select
+                                          className="form-control"
+                                          onChange={(e) => {
+                                            setRoleMenuParentID(e.target.value);
+                                          }}
+                                      >
+                                          <option>--เลือก Role--</option>
+                                          {rolegroup.map((item) => (
+                                              <option key={item.id} value={item.id}>
+                                                  {item.role_group_name}
+                                              </option>
+                                          ))}
+                                      </select>
+                                  </div>   
+                                  <br />
+                                  <div>                              
+                                      <Button variant="success">
+                                          เพิ่มเมนูหลัก
                                       </Button>
                                   </div>
                                   <hr />
