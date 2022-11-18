7864c326ef3063d9ace72ac2ca2f023c46fe3676
PATIPAN12042541 <patiphan@lobo.co.th>
2022-11-18 16:34:48 +0700

    commit

---
+++ backend/models/HarvestLogTransection_M.js
@@ -0,0 +1,26 @@
+import { Sequelize } from "sequelize";
+import db from "../config/Database.js";
+
+const { DataTypes } = Sequelize;
+const HarvestLogTransection = db.define(
+  "harvest_log_transection",
+  {
+    id_plant: {
+      type: DataTypes.INTEGER,
+    },
+    id_plant: {
+      type: DataTypes.INTEGER,
+    },
+    time_log: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+(async () => {
+  await db.sync();
+})();
+export default HarvestLogTransection;
