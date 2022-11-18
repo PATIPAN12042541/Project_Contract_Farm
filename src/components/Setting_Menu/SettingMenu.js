ef84dd8cf7ddecbe613269adb849d423ebbe2bd7
kobkij <Kobkij@globofoods.com>
2022-11-18 15:16:59 +0700

    commit

--- src/components/Setting_Menu/SettingMenu.js
+++ src/components/Setting_Menu/SettingMenu.js
@@ -2,26 +2,33 @@ import React from 'react'
 
 const SettingMenu = () => {
   return (
-    <div className="content-wrapper">
-    <section className="content-header">
-      <div className="container-fluid">
-        <div className="row mb-2">
-          <div className="col-sm-12"></div>
-        </div>
+      <div className="content-wrapper">
+          <section className="content-header">
+              <div className="container-fluid">
+                  <div className="row mb-2">
+                      <div className="col-sm-12"></div>
+                  </div>
+              </div>
+          </section>
+          <section className="content">
+              <div className="container-fluid">
+                  <div className="row">
+                      <div className="col-md-12">
+                          <div className="card card-success">
+                              <div
+                                  className="card-header"
+                                  style={{ backgroundColor: "#8CC152" }}
+                              >
+                                  <center>
+                                      <h3 className="card-title">ตั้งค่าเมนู</h3>
+                                  </center>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </section>
       </div>
-    </section>
-    <section className="content">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-12">
-            <div className="card card-success">
-                 
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </div>
   )
 }
 export default SettingMenu
