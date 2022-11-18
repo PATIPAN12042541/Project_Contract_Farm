4afa5401cb422c752114f2758fce34a77708c583
kobkij <Kobkij@globofoods.com>
2022-11-18 15:09:13 +0700

    commit

--- src/components/Setting_Menu/SettingMenu.js
+++ src/components/Setting_Menu/SettingMenu.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const SettingMenu = () => {
   return (
-    <div>SettingMenu</div>
+    <div className="content-wrapper">
+    <section className="content-header">
+      <div className="container-fluid">
+        <div className="row mb-2">
+          <div className="col-sm-12"></div>
+        </div>
+      </div>
+    </section>
+    <section className="content">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card card-success">
+                 
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
   )
 }
 export default SettingMenu
