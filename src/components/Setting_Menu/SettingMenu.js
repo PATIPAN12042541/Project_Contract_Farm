53c466e3be4996f3e87f92ac7d8786daa9c6056b
kobkij <Kobkij@globofoods.com>
2022-11-18 15:25:21 +0700

    commit

--- src/components/Setting_Menu/SettingMenu.js
+++ src/components/Setting_Menu/SettingMenu.js
@@ -23,6 +23,35 @@ const SettingMenu = () => {
                                       <h3 className="card-title">ตั้งค่าเมนู</h3>
                                   </center>
                               </div>
+                              <div className="card-body">
+                                  <div className="row">
+                                      <Button variant="success">
+                                          เพิมเมนูหลัก
+                                      </Button>
+                                  </div>
+                                  <hr />
+                                  <div className="row">
+                                      <Table
+                                          className="table table-bordered table-hover dataTable dtr-inline"
+                                          responsive
+                                      >
+                                          <thead>
+                                              <tr>
+                                                  <th>ลำดับ</th>
+                                                  <th>ชื่อเมนู</th>
+                                                  <th>ลำดับของเมนู</th>
+                                                  <th>Link</th>
+                                                  <th>
+                                                      <center>แก้ไขข้อมูล</center>
+                                                  </th>
+                                                  <th>Active</th>
+                                              </tr>
+                                          </thead>
+                                          <tbody>
+                                          </tbody>
+                                      </Table>
+                                  </div>
+                              </div>
                           </div>
                       </div>
                   </div>
