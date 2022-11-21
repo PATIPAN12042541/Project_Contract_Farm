4b771a07bc3c16bd6d82df2fa6ad3fbe1e014af2
kobkij <Kobkij@globofoods.com>
2022-11-21 09:36:39 +0700

    commit

--- backend/controllers/Menus.js
+++ backend/controllers/Menus.js
@@ -20,6 +20,24 @@ export const getMenusRoleMain = async (req, res) => {
     } 
 }
 
+export const getMenusRoleMainAll = async (req, res) => {
+    try {
+        const menus = await Menus.findAll({
+            where:{
+                parent_id:0,
+                status:1,
+            },
+            order: [
+                ['parent_id','asc'],
+                ['index_menu','asc'],
+            ]
+        })
+        res.json(menus);
+    } catch (error) {
+        res.json({ message: error.message });
+    } 
+}
+
 export const getMenusRoleSubLV1 = async (req, res) => {
     try {
         // const menus = await Menus.findAll({
--- backend/routes/MenuRoleRoute.js
+++ backend/routes/MenuRoleRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {getMenusRoleMain,
-       getMenusRoleSubLV1
+       getMenusRoleSubLV1,
+       getMenusRoleMainAll
        } from "../controllers/Menus.js";
 
 const router = express.Router();
+router.get('/', getMenusRoleMainAll);
 router.get('/main/:role_id', getMenusRoleMain);
 router.get('/sublv1/:role_id', getMenusRoleSubLV1);
 
