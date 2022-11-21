3ad4358e7f5e2d91354d627654420157601db532
kobkij <Kobkij@globofoods.com>
2022-11-21 13:18:19 +0700

    commit

--- src/components/User/List_User.js
+++ src/components/User/List_User.js
@@ -94,7 +94,7 @@ export const List_User = () => {
                 `${process.env.REACT_APP_API_URL}/User/getUsersByDev`
             );
             setListUsers(response.data);
-            console.log(response.data)
+            console.log(listUsers)
         }else{
             const response = await axios.get(
                 `${process.env.REACT_APP_API_URL}/User/getUsersByAdmin`
