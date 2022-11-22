c22b1c740d34f26eaf2d83713e8d27d9eb3a234d
kobkij <Kobkij@globofoods.com>
2022-11-22 11:32:34 +0700

    commit

--- src/components/Setting_Menu/SettingMenu.js
+++ src/components/Setting_Menu/SettingMenu.js
@@ -421,7 +421,6 @@ const SettingMenu = () => {
                                                                       onClick={(e)=>{
                                                                         handleShowUpdateSubMenu();
                                                                         getMenuInDropDownUpdateSubMenu(roleMenuParent.role_id);
-                                                                        getMenuInDropDownUpdateSubMenu(roleMenuParent.role_id);
                                                                         setUpdateSubMenuID(roleMenuParent.id);
                                                                         setUpdateSubMenuName(roleMenuParent.menu_name);
                                                                         setUpdateIndexSubMenu(roleMenuParent.index_menu);
