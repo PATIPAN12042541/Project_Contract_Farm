6c509eb185c7d48433a2ca12bd360ff6f02cbc6b
kobkij <Kobkij@globofoods.com>
2022-11-22 10:55:13 +0700

    commit

--- src/components/Setting_Menu/SettingMenu.js
+++ src/components/Setting_Menu/SettingMenu.js
@@ -833,6 +833,7 @@ const SettingMenu = () => {
                                   <select
                                       className="form-control"
                                       defaultValue={updateSubRoleID}
+                                      onSelect={console.log("updateSubRoleID : "+updateSubRoleID)}
                                       onChange={(e)=>{
                                         setUpdateSubRoleID(e.target.value);
                                         getMenuInDropDownUpdateSubMenu(e.target.value);
