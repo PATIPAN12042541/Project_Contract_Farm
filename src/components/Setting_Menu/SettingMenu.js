4b3793648b78180573791cb7d80ea8fa4f315e8b
kobkij <Kobkij@globofoods.com>
2022-11-22 15:08:36 +0700

    commit

--- src/components/Setting_Menu/SettingMenu.js
+++ src/components/Setting_Menu/SettingMenu.js
@@ -205,7 +205,7 @@ const SettingMenu = () => {
                     index_menu : insertIndexMainMenu,
                     parent_id : 0,
                     link : insertMainLink,
-                    status : checkedAddMainMenu,
+                    status : checkedAddMainMenu=true?1:0,
                     role_id : insertMainRoleId
                 })
                 .then(function (response) {
