8dbed9f22bf0b26e46be0f7ab66922f7cec4b15e
kobkij <Kobkij@globofoods.com>
2022-11-24 13:59:03 +0700

    commit

--- backend/index.js
+++ backend/index.js
@@ -118,21 +118,21 @@ try {
   //   }
   // );
 
-  app.post("/public/dist/img/UploadWorking", upload_4.single("file"),async (req, res) => {
-    const { filename: file } = req.file;
+  app.post("/public/dist/img/UploadWorking", upload_4.single('image'),async (req, res) => {
+    const { filename: image } = req.file;
 
     console.log("req : "+req)
     console.log("res : "+res)
 
-    // await sharp(req.file.path)
-    //  .resize(200, 200)
-    //  .jpeg({ quality: 90 })
-    //  .toFile(
-    //      path.resolve(req.file.destination,'resized',file)
-    //  )
-    //  fs.unlinkSync(req.file.path)
-   
-    // res.redirect('/');
+    await sharp(req.file.path)
+        .resize(200, 200)
+        .jpeg({ quality: 90 })
+        .toFile(
+            path.resolve(req.file.destination,'resized',image)
+        )
+        fs.unlinkSync(req.file.path)
+      
+       res.redirect('/');
 });
 } catch (error) {
   res.json(console.log("Upload 4 Fail"));
